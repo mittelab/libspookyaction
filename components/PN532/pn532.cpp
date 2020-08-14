90202803e4b4984c2aba50677a237069c8a1bda5
Luca Cristaldi <wifasoi@gmail.com>
2020-08-14 13:56:31 +0200

    Implementazione comandi base pn532 (TBT)

--- components/PN532/pn532.cpp
+++ components/PN532/pn532.cpp
@@ -3,16 +3,17 @@
 #define PN532_DEFAULT_TIMEOUT (1000/portTICK_PERIOD_MS)
 
 
+
 template<class T>
 void PN532<T>::begin(TickType_t timeout)
 {
     BaseType_t tWrite = xTaskGetTickCount();
     T::wake_up(xTaskGetTickCount() - tWrite);
-    sam_config(xTaskGetTickCount() - tWrite);
+    sam_config(normal_mode,0x14,0x01,xTaskGetTickCount() - tWrite);
 }
 
 template<class T>
-int PN532<T>::cmd(const uint8_t cmd, const std::vector<uint8_t> param, TickType_t timeout)
+int PN532<T>::cmd(const uint8_t cmd, const std::vector<uint8_t>& param, TickType_t timeout)
 {
     BaseType_t tWrite = xTaskGetTickCount();
     T::send(cmd, param, xTaskGetTickCount() - tWrite);
@@ -20,29 +21,137 @@ int PN532<T>::cmd(const uint8_t cmd, const std::vector<uint8_t> param, TickType_
 }
 
 template<class T>
-int PN532<T>::sam_config(TickType_t timeout)
+int PN532<T>::sam_config(SAM_mode mode, uint8_t time, uint8_t irq, TickType_t timeout)
 {
     ESP_LOGI(PN532_LOG, "Configuring pn532 SAM as not used");
     const std::vector<uint8_t> ackbuff = {
-        0x01,   // normal mode
-        0x14,   // timeout 50ms * 20 = 1 second
-        0x01    // use IRQ pin
+        mode,   // normal mode
+        time,   // timeout 50ms * 20 = 1 second
+        irq    // use IRQ pin
     };
     return cmd(PN532_COMMAND_SAMCONFIGURATION, ackbuff, timeout);
 
 }
 
 template<class T>
-int PN532<T>::data_exchange(const uint8_t command, const std::vector<uint8_t> param, std::vector<uint8_t> data, TickType_t timeout)
+int PN532<T>::read(const uint8_t command, std::vector<uint8_t>& data, TickType_t timeout)
+{
+    BaseType_t tWrite = xTaskGetTickCount();
+    if(cmd(command, std::vector<uint8_t>(), xTaskGetTickCount() - tWrite) >= 0)
+        return T::receive(data, xTaskGetTickCount() - tWrite);
+}
+
+template<class T>
+int PN532<T>::data_exchange(const uint8_t command, const std::vector<uint8_t>& param, std::vector<uint8_t>& data, TickType_t timeout)
 {
     BaseType_t tWrite = xTaskGetTickCount();
     if(cmd(command, param, xTaskGetTickCount() - tWrite) >= 0)
         return T::receive(data, xTaskGetTickCount() - tWrite);
 }
 
-// int PN532::wake_up(TickType_t timeout = PN532_DEFAULT_TIMEOUT)
-// {
-//     const char buffer[] = {0x55, 0x55, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x02, 0xfe, 0xd4, 0x02, 0x2a, 0x00};
-//     uart_write_bytes(port, buffer, sizeof(buffer));
-//     return pn532_serial_readack(port, 1000/portTICK_PERIOD_MS);
-//     return ESP_OK;
+// template<class T>
+// int PN532<T>::Diagnose(){}
+
+template<class T>
+pn532_info_t PN532<T>::getFirmwareVersion(TickType_t timeout)
+{   
+    std::vector<uint8_t> version;
+    data_exchange(PN532_COMMAND_GETFIRMWAREVERSION, std::vector<uint8_t>(), version, timeout);
+    pn532_info_t data;
+    std::copy(version.begin(), version.end(), &data);
+    return data;
+}
+
+// template<class T>
+// int PN532<T>::getGeneralStatus(){}
+
+template<class T>
+uint8_t PN532<T>::readRegister(const uint16_t address ,TickType_t timeout)
+{   
+    const std::vector<uint8_t> adr = {
+        (uint8_t) (address & 0xFF00) >> 8,
+        (uint8_t) (address & 0x00FF)
+    };
+    std::vector<uint8_t> data;
+    data_exchange(PN532_COMMAND_READREGISTER,adr,data,timeout);
+    return data[0];
+}
+
+// template<class T>
+// int PN532<T>::readRegister(const std::vector<uint16_t>& adresses ,TickType_t timeout)
+// {}
+
+
+template<class T>
+void PN532<T>::writeRegister(const uint16_t address, const uint8_t data ,TickType_t timeout)
+{   
+    const std::vector<uint8_t> packet = {
+        (uint8_t) (address & 0xFF00) >> 8,
+        (uint8_t) (address & 0x00FF),
+        data
+    };
+    cmd(PN532_COMMAND_WRITEREGISTER,packet,timeout);
+}
+// template<class T>
+// int PN532<T>::writeRegister(){}
+
+template<class T>
+void PN532<T>::readGpio(gpio_t& gpio, TickType_t timeout)
+{   
+    std::vector<uint8_t> data;
+    data_exchange(PN532_COMMAND_READGPIO,std::vector<uint8_t>(), data);
+    std::copy(data.begin(), data.end(), gpio);
+}
+
+template<class T>
+void PN532<T>::writeGpio(gpio_t& gpio, TickType_t timeout)
+{
+    auto ptr = reinterpret_cast<uint8_t*>(gpio);
+    std::vector<uint8_t> data(ptr, ptr + sizeof gpio);
+    cmd(PN532_COMMAND_READGPIO,std::vector<uint8_t>(), data);
+}
+
+// template<class T>
+// int PN532<T>::setSerialBaudrate(){}
+
+// template<class T>
+// int PN532<T>::setParameters(){}
+
+// template<class T>
+// int PN532<T>::powerDown(){}
+
+// template<class T>
+// int PN532<T>::rfConfiguration(){}
+
+// template<class T>
+// int PN532<T>::InJumpForDEP(){}
+
+// template<class T>
+// int PN532<T>::InJumpForPSL(){}
+
+// template<class T>
+// int PN532<T>::InListPassiveTarget(){}
+
+// template<class T>
+// int PN532<T>::InATR(){}
+
+// template<class T>
+// int PN532<T>::InPSL(){}
+
+// template<class T>
+// int PN532<T>::InDataExchange(){}
+
+// template<class T>
+// int PN532<T>::InCommunicateThru(){}
+
+// template<class T>
+// int PN532<T>::InDeselect(){}
+
+// template<class T>
+// int PN532<T>::InRelease(){}
+
+// template<class T>
+// int PN532<T>::InSelect(){}
+
+// template<class T>
+// int PN532<T>::InAutoPoll(){}
--- components/PN532/pn532.hpp
+++ components/PN532/pn532.hpp
@@ -6,16 +6,68 @@
 #include <stddef.h>
 #include "instructions.hpp"
 
+enum SAM_mode : uint8_t{
+    normal_mode=0x01,
+    virtual_card=0x02,
+    wired_card=0x03,
+    dual_card=0x04
+};
+
+enum diagnose: uint8_t{
+    comunication_test=0x00,
+    rom_test=0x01,
+    ram_test=0x02,
+    polling_test=0x04,
+    echo_test=0x05,
+    attention_request_test=0x06,
+    slef_antenna_test=0x07
+};
+
+typedef struct{
+    uint8_t :5;
+    bool iso14443b:1;
+    bool iso14443a:1;
+    bool iso18092:1;
+} card_t;
+
+typedef struct{
+    uint8_t ic_version;
+    uint8_t firmware_version;
+    uint8_t firmware_revision;
+    card_t card_supported;
+} pn532_info_t;
+
+typedef struct{
+    bool P30:1, P31:1,P32:1,P33:1,P34:1,P35:1,:2;
+} p3_t;
+
+typedef struct{
+    bool :1,P71:1, P72:1,:5;
+}p7_t;
+
+typedef struct{
+    p3_t P3;
+    p7_t P7;
+} gpio_t;
+
 
 template<class T>
-class PN532 : public T{
+class PN532 : private T{
     public:
         using T::T;
         void begin(TickType_t timeout = PN532_DEFAULT_TIMEOUT);
-        int cmd(const uint8_t cmd, const std::vector<uint8_t> param = {}, TickType_t timeout = PN532_DEFAULT_TIMEOUT);
-        int data_exchange(const uint8_t command, const std::vector<uint8_t> param, std::vector<uint8_t> data, TickType_t timeout = PN532_DEFAULT_TIMEOUT);
-        int sam_config(TickType_t timeout = PN532_DEFAULT_TIMEOUT);
+        int cmd(const uint8_t cmd, const std::vector<uint8_t>& param = {}, TickType_t timeout = PN532_DEFAULT_TIMEOUT);
+        int read(const uint8_t command, std::vector<uint8_t>& data, TickType_t timeout = PN532_DEFAULT_TIMEOUT);
+        int data_exchange(const uint8_t command, const std::vector<uint8_t>& param, std::vector<uint8_t>& data, TickType_t timeout = PN532_DEFAULT_TIMEOUT);
+        int sam_config(SAM_mode mode=normal_mode, uint8_t time=0x14, uint8_t irq=0x01, TickType_t timeout = PN532_DEFAULT_TIMEOUT);
         //int wake_up(TickType_t timeout);
+
+        pn532_info_t getFirmwareVersion(TickType_t timeout = PN532_DEFAULT_TIMEOUT);
+        uint8_t readRegister(const uint16_t address ,TickType_t timeout = PN532_DEFAULT_TIMEOUT);
+        void writeRegister(const uint16_t address, const uint8_t data ,TickType_t timeout = PN532_DEFAULT_TIMEOUT);
+        void readGpio(gpio_t& gpio, TickType_t timeout);
+        void writeGpio(gpio_t& gpio, TickType_t timeout);
+
 };
 #include "pn532.cpp"
 #endif

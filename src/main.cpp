30aa7341b9290457f983dde78c046deeb6b1a121
Pietro Saccardi <lizardm4@gmail.com>
2020-12-25 21:39:04 +0100

    Running diagnostic tests

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@
 #define RX_PIN   (GPIO_NUM_16)
 #define BUF_SIZE (1024)
 
+namespace {
+    std::unique_ptr<pn532::hsu> serial = nullptr;
+    std::unique_ptr<pn532::nfc> tag_reader = nullptr;
+}
+
 void setup_uart()
 {
     uart_config_t uart_config = {
@@ -26,15 +31,32 @@ void setup_uart()
     uart_set_pin(UART_NUM_1, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 }
 
+template <class T, class ...Args>
+std::unique_ptr<T> make_unique(Args &&...args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+bool is_ok(pn532::nfc::r<bool> const &r) {
+    return r and *r;
+}
+
 void test_get_fw()
 {
-    pn532::hsu serial{UART_NUM_1};
-    pn532::nfc tag_reader{serial};
-    serial.wake();
-    tag_reader.sam_configuration(pn532::sam_mode::normal, pn532::one_sec);
-    const auto r_fw = tag_reader.get_firmware_version();
+    serial = make_unique<pn532::hsu>(UART_NUM_1);
+    tag_reader = make_unique<pn532::nfc>(*serial);
+
+    serial->wake();
+    tag_reader->sam_configuration(pn532::sam_mode::normal, pn532::one_sec);
+    const auto r_fw = tag_reader->get_firmware_version();
     TEST_ASSERT(bool(r_fw));
     ESP_LOGI(TEST_TAG, "IC version %u, version: %u.%u", r_fw->ic, r_fw->version, r_fw->revision);
+}
+
+void test_diagnostics() {
+    TEST_ASSERT(is_ok(tag_reader->diagnose_rom()));
+    TEST_ASSERT(is_ok(tag_reader->diagnose_ram()));
+    TEST_ASSERT(is_ok(tag_reader->diagnose_comm_line()));
+    TEST_ASSERT(is_ok(tag_reader->diagnose_self_antenna(0x7f)));
 }
 
 extern "C" void app_main()
@@ -42,6 +64,7 @@ extern "C" void app_main()
     UNITY_BEGIN();
     RUN_TEST(setup_uart);
     RUN_TEST(test_get_fw);
+    RUN_TEST(test_diagnostics);
     UNITY_END();
 }
 

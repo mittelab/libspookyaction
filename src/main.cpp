66c53360b3e7537c6fc8fc9ef53b9fc764a30287
Pietro Saccardi <lizardm4@gmail.com>
2021-01-05 02:09:08 +0100

    Move setup and add third stage in test_cipher_des

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ namespace {
 
 }
 
-void setup_uart() {
+void setup_uart_pn532() {
     uart_config_t uart_config = {
             .baud_rate = 115200,
             .data_bits = UART_DATA_8_BITS,
@@ -45,15 +45,16 @@ void setup_uart() {
     uart_param_config(UART_NUM_1, &uart_config);
     uart_driver_install(UART_NUM_1, BUF_SIZE, BUF_SIZE, 0, nullptr, 0);
     uart_set_pin(UART_NUM_1, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    serial = make_unique<pn532::hsu>(UART_NUM_1);
+    tag_reader = make_unique<pn532::nfc>(*serial);
+    serial->wake();
+    const auto r_sam = tag_reader->sam_configuration(pn532::sam_mode::normal, pn532::one_sec);
+    TEST_ASSERT(bool(r_sam));
 }
 
 
 void test_get_fw() {
-    serial = make_unique<pn532::hsu>(UART_NUM_1);
-    tag_reader = make_unique<pn532::nfc>(*serial);
-
-    serial->wake();
-    tag_reader->sam_configuration(pn532::sam_mode::normal, pn532::one_sec);
     const auto r_fw = tag_reader->get_firmware_version();
     TEST_ASSERT(bool(r_fw));
     ESP_LOGI(TEST_TAG, "IC version %u, version: %u.%u", r_fw->ic, r_fw->version, r_fw->revision);
@@ -174,6 +175,12 @@ void test_cipher_des() {
         c.prepare_tx(dec_data, 0, desfire::cipher_cfg_crypto_nocrc);
         TEST_ASSERT(enc_data == dec_data);
     }
+    {
+        desfire::bin_data enc_data = {0x91, 0x3C, 0x6D, 0xED, 0x84, 0x22, 0x1C, 0x41, /* status */ 0x00};
+        const desfire::bin_data dec_data = {0x9B, 0x36, 0xC5, 0xF8, 0xBF, 0x4A, 0x09, 0x84, /* status */ 0x00};
+        c.confirm_rx(enc_data, desfire::cipher_cfg_crypto_nocrc);
+        TEST_ASSERT(enc_data == dec_data);
+    }
 }
 
 void test_cipher_2k3des() {
@@ -244,7 +251,7 @@ void issue_header(std::string const &title) {
 extern "C" void app_main() {
     UNITY_BEGIN();
     issue_header("HARDWARE SETUP");
-    RUN_TEST(setup_uart);
+    RUN_TEST(setup_uart_pn532);
     issue_header("PN532 TEST AND DIAGNOSTICS");
     RUN_TEST(test_get_fw);
     RUN_TEST(test_diagnostics);

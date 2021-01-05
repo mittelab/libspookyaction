bd90555131a89fb3478774056d7c8ea106429ec4
Pietro Saccardi <lizardm4@gmail.com>
2021-01-05 01:15:23 +0100

    Bugfix, check rf_status before returning true

--- src/pn532/desfire_pcd.cpp
+++ src/pn532/desfire_pcd.cpp
@@ -9,7 +9,8 @@ namespace pn532 {
         auto res = pcd().initiator_data_exchange(target_logical_index(), data);
         if (res) {
             _last_result = nfc::r<rf_status>{res->first};
-            return {std::move(res->second), true};
+            // Check also the RF status
+            return {std::move(res->second), res->first.error == controller_error::none};
         } else {
             _last_result = nfc::r<rf_status>{res.error()};
             return {bin_data{}, false};

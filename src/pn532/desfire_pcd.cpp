114beab00f93231cc8e193d50010bd70bfa73ac5
Pietro Saccardi <lizardm4@gmail.com>
2021-01-30 15:23:33 +0100

    Log in case of error

--- src/pn532/desfire_pcd.cpp
+++ src/pn532/desfire_pcd.cpp
@@ -9,9 +9,13 @@ namespace pn532 {
         auto res = pcd().initiator_data_exchange(target_logical_index(), data);
         if (res) {
             _last_result = nfc::r<rf_status>{res->first};
+            if (res->first.error != controller_error::none) {
+                PN532_LOGE("PCD/PICC comm failed at protocol level, %s", to_string(res->first.error));
+            }
             // Check also the RF status
             return {std::move(res->second), res->first.error == controller_error::none};
         } else {
+            PN532_LOGE("PCD/PICC comm failed at NFC level, %s", to_string(res.error()));
             _last_result = nfc::r<rf_status>{res.error()};
             return {bin_data{}, false};
         }

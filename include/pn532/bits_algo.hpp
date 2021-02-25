021521c7fbbcc8cf981124f4535f796427966dfc
Pietro Saccardi <lizardm4@gmail.com>
2021-02-25 02:22:06 +0100

    Minor changes to namespace declaration

--- include/pn532/bits_algo.hpp
+++ include/pn532/bits_algo.hpp
@@ -10,83 +10,79 @@
 #include <cstdint>
 #include <numeric>
 
-namespace pn532 {
-    namespace bits {
+namespace pn532::bits {
 
-        inline std::uint8_t compute_checksum(std::uint8_t byte);
+    [[nodiscard]] inline std::uint8_t compute_checksum(std::uint8_t byte);
 
-        template <class ByteIterator>
-        std::uint8_t compute_checksum(ByteIterator begin, ByteIterator end);
+    template <class ByteIterator>
+    [[nodiscard]] std::uint8_t compute_checksum(ByteIterator begin, ByteIterator end);
 
-        template <class ByteIterator>
-        std::uint8_t compute_checksum(std::uint8_t sum_init, ByteIterator begin, ByteIterator end);
+    template <class ByteIterator>
+    [[nodiscard]] std::uint8_t compute_checksum(std::uint8_t sum_init, ByteIterator begin, ByteIterator end);
 
-        template <class ByteIterator>
-        bool checksum(ByteIterator begin, ByteIterator end);
+    template <class ByteIterator>
+    [[nodiscard]] bool checksum(ByteIterator begin, ByteIterator end);
 
-        inline std::array<std::uint8_t, 2> length_and_checksum_short(std::uint8_t length);
+    [[nodiscard]] inline std::array<std::uint8_t, 2> length_and_checksum_short(std::uint8_t length);
 
-        inline std::array<std::uint8_t, 3> length_and_checksum_long(std::uint16_t length);
+    [[nodiscard]] inline std::array<std::uint8_t, 3> length_and_checksum_long(std::uint16_t length);
 
-        inline std::pair<std::uint8_t, bool> check_length_checksum(std::array<std::uint8_t, 2> const &data);
+    [[nodiscard]] inline std::pair<std::uint8_t, bool> check_length_checksum(std::array<std::uint8_t, 2> const &data);
 
-        inline std::pair<std::uint16_t, bool> check_length_checksum(std::array<std::uint8_t, 3> const &data);
+    [[nodiscard]] inline std::pair<std::uint16_t, bool> check_length_checksum(std::array<std::uint8_t, 3> const &data);
 
-        inline std::uint8_t host_to_pn532_command(command cmd);
+    [[nodiscard]] inline std::uint8_t host_to_pn532_command(command cmd);
 
-        inline command pn532_to_host_command(std::uint8_t cmd);
+    [[nodiscard]] inline command pn532_to_host_command(std::uint8_t cmd);
 
-    }// namespace bits
-}// namespace pn532
+}// namespace pn532::bits
 
-namespace pn532 {
-    namespace bits {
+namespace pn532::bits {
 
-        std::uint8_t compute_checksum(std::uint8_t byte) {
-            return ~byte + 1;
-        }
+    std::uint8_t compute_checksum(std::uint8_t byte) {
+        return ~byte + 1;
+    }
 
-        template <class ByteIterator>
-        std::uint8_t compute_checksum(ByteIterator begin, ByteIterator end) {
-            return compute_checksum(0, begin, end);
-        }
+    template <class ByteIterator>
+    std::uint8_t compute_checksum(ByteIterator begin, ByteIterator end) {
+        return compute_checksum(0, begin, end);
+    }
 
-        template <class ByteIterator>
-        std::uint8_t compute_checksum(std::uint8_t sum_init, ByteIterator begin, ByteIterator end) {
-            return compute_checksum(std::accumulate(begin, end, sum_init));
-        }
+    template <class ByteIterator>
+    std::uint8_t compute_checksum(std::uint8_t sum_init, ByteIterator begin, ByteIterator end) {
+        return compute_checksum(std::accumulate(begin, end, sum_init));
+    }
 
-        template <class ByteIterator>
-        bool checksum(ByteIterator begin, ByteIterator end) {
-            return (std::accumulate(begin, end, 0) & 0xff) == 0;
-        }
+    template <class ByteIterator>
+    bool checksum(ByteIterator begin, ByteIterator end) {
+        return (std::accumulate(begin, end, 0) & 0xff) == 0;
+    }
 
-        std::array<std::uint8_t, 2> length_and_checksum_short(std::uint8_t length) {
-            return {length, compute_checksum(length)};
-        }
+    std::array<std::uint8_t, 2> length_and_checksum_short(std::uint8_t length) {
+        return {length, compute_checksum(length)};
+    }
 
-        std::array<std::uint8_t, 3> length_and_checksum_long(std::uint16_t length) {
-            const std::array<std::uint8_t, 2> bits = {std::uint8_t(length >> 8), std::uint8_t(length & 0xff)};
-            return {bits[0], bits[1], compute_checksum(std::begin(bits), std::end(bits))};
-        }
+    std::array<std::uint8_t, 3> length_and_checksum_long(std::uint16_t length) {
+        const std::array<std::uint8_t, 2> bits = {std::uint8_t(length >> 8), std::uint8_t(length & 0xff)};
+        return {bits[0], bits[1], compute_checksum(std::begin(bits), std::end(bits))};
+    }
 
-        std::pair<std::uint8_t, bool> check_length_checksum(std::array<std::uint8_t, 2> const &data) {
-            return {data[0], checksum(std::begin(data), std::end(data))};
-        }
+    std::pair<std::uint8_t, bool> check_length_checksum(std::array<std::uint8_t, 2> const &data) {
+        return {data[0], checksum(std::begin(data), std::end(data))};
+    }
 
-        std::pair<std::uint16_t, bool> check_length_checksum(std::array<std::uint8_t, 3> const &data) {
-            return {(std::uint16_t(data[0]) << 8) | data[1],
-                    checksum(std::begin(data), std::end(data))};
-        }
+    std::pair<std::uint16_t, bool> check_length_checksum(std::array<std::uint8_t, 3> const &data) {
+        return {(std::uint16_t(data[0]) << 8) | data[1],
+                checksum(std::begin(data), std::end(data))};
+    }
 
-        inline std::uint8_t host_to_pn532_command(command cmd) {
-            return static_cast<std::uint8_t>(cmd);
-        }
+    inline std::uint8_t host_to_pn532_command(command cmd) {
+        return static_cast<std::uint8_t>(cmd);
+    }
 
-        inline command pn532_to_host_command(std::uint8_t cmd) {
-            return static_cast<command>(cmd - 1);
-        }
-    }// namespace bits
-}// namespace pn532
+    inline command pn532_to_host_command(std::uint8_t cmd) {
+        return static_cast<command>(cmd - 1);
+    }
+}// namespace pn532::bits
 
 #endif//PN532_BITS_ALGO_HPP

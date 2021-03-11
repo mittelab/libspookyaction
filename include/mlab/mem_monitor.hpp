387a4182436cba29922ce4e6a92e7d1d84651496
Pietro Saccardi <lizardm4@gmail.com>
2021-03-11 21:59:51 +0100

    Use ESP32's heap monitor

--- include/mlab/mem_monitor.hpp
+++ include/mlab/mem_monitor.hpp
@@ -2,36 +2,59 @@
 // Created by spak on 3/10/21.
 //
 
-#ifndef _GUARD_MLAB_MEM_MONITOR_HPP
-#define _GUARD_MLAB_MEM_MONITOR_HPP
-#include <esp_system.h>
+#ifndef MLAB_MEM_MONITOR_HPP
+#define MLAB_MEM_MONITOR_HPP
+
+#include <esp_heap_trace.h>
 
 namespace mlab {
+    namespace trace {
+        static constexpr std::size_t num_records = 100;
+        static heap_trace_record_t records[num_records]; // This buffer must be in internal RAM
+    }
 
     struct mem_monitor {
-        const char *msg;
-        std::int_least64_t expected_mem;
-        inline void reduce(std::size_t expected_leak);
-        inline explicit mem_monitor(const char *msg_);
+        std::size_t expected_leak = 0;
+        inline mem_monitor();
         inline ~mem_monitor();
+        mem_monitor(mem_monitor &&) noexcept = delete;
+        mem_monitor(mem_monitor const &) = delete;
+        mem_monitor & operator=(mem_monitor &&) noexcept = delete;
+        mem_monitor & operator=(mem_monitor const &) = delete;
+        [[nodiscard]] inline std::size_t count_leaked_memory() const;
     };
 
 }
 
 namespace mlab {
 
-    void mem_monitor::reduce(std::size_t expected_leak) {
-        expected_mem -= expected_leak;
+    mem_monitor::mem_monitor() : expected_leak{0} {
+        ESP_LOGI("MEM", "Begin heap monitoring");
+        ESP_ERROR_CHECK(heap_trace_init_standalone(trace::records, trace::num_records));
+        ESP_ERROR_CHECK(heap_trace_start(HEAP_TRACE_LEAKS));
     }
-
-    mem_monitor::mem_monitor(const char *msg_) : msg{msg_}, expected_mem{esp_get_free_heap_size()} {}
 
     mem_monitor::~mem_monitor() {
-        const std::int_least64_t actual_mem = esp_get_free_heap_size();
-        if (actual_mem != expected_mem) {
-            ESP_LOGW("MEM", "%s: %+lld", msg, actual_mem - std::int_least64_t(expected_mem));
+        ESP_ERROR_CHECK(heap_trace_stop());
+        if (const auto leaked = count_leaked_memory(); leaked != expected_leak) {
+            ESP_LOGW("MEM", "End heap monitoring, expected leak: %d, actual leak: %d", expected_leak, leaked);
+            heap_trace_dump();
+        } else {
+            ESP_LOGI("MEM", "End heap monitoring, no unexpected leak.");
         }
+        ESP_ERROR_CHECK(heap_trace_init_standalone(nullptr, 0));
     }
+
+    std::size_t mem_monitor::count_leaked_memory() const {
+        std::size_t leaked = 0;
+        for (std::size_t i = 0; i < heap_trace_get_count(); ++i) {
+            heap_trace_record_t record{};
+            heap_trace_get(i, &record);
+            leaked += record.size;
+        }
+        return leaked;
+    }
+
 }
 
-#endif//_GUARD_MLAB_MEM_MONITOR_HPP
+#endif//MLAB_MEM_MONITOR_HPP

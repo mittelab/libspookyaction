6a3ecfd4c8a6427622159b509a55ca7344c49698
Pietro Saccardi <lizardm4@gmail.com>
2021-03-15 00:29:43 +0100

    Make irq_assert movable and default constructible

--- include/mlab/irq_assert.hpp
+++ include/mlab/irq_assert.hpp
@@ -14,8 +14,28 @@ namespace mlab {
         struct impl;
         std::unique_ptr<impl> _pimpl;
     public:
+        /**
+         * Default constructor: always asserts `true` at @ref ::operator()
+         */
+        irq_assert();
+
+        irq_assert(irq_assert &&) noexcept;
+        irq_assert &operator=(irq_assert &&) noexcept;
+
+        irq_assert(irq_assert const &) noexcept = delete;
+        irq_assert &operator=(irq_assert const &) noexcept = delete;
+
         explicit irq_assert(bool manage_isr_service, gpio_num_t pin, gpio_int_type_t interrupt_type = GPIO_INTR_NEGEDGE);
+
+        [[nodiscard]] gpio_num_t pin() const;
+        /**
+         * Waits at most @p timeout and returns true if and only if the given pin asserts the interrupt.
+         * @param timeout Maximum timeout before returning `false`.
+         * @return True if the interrupt specified at runtime has been triggered on the given pin.
+         * @note Always asserts true if @ref irq_assert was default constructed.
+         */
         bool operator()(ms timeout);
+
         ~irq_assert();
     };
 
--- src/mlab/irq_assert.cpp
+++ src/mlab/irq_assert.cpp
@@ -22,6 +22,30 @@ namespace mlab {
         gpio_num_t pin = GPIO_NUM_MAX;
     };
 
+    /**
+     * @addtogroup PIMPL
+     * These implementations, despite being default, must appear after @ref irq_assert::impl is complete, because
+     * they require the corresponding operators/constructors from `unique_ptr`, and those are only defined once
+     * the type is complete. As a workaround, the `unique_ptr` could be created with an erased deleter (e.g.
+     * some `std::default_delete<irq_assert::impl>` erased as `std::function<void(void *)>`, but is not worth it.
+     * @{
+     */
+    irq_assert::irq_assert() = default;
+
+    irq_assert::irq_assert(irq_assert &&) noexcept = default;
+
+    irq_assert &irq_assert::operator=(irq_assert &&) noexcept = default;
+    /**
+     * @}
+     */
+
+    gpio_num_t irq_assert::pin() const {
+        if (_pimpl) {
+            return _pimpl->pin;
+        }
+        return GPIO_NUM_MAX;
+    }
+
     irq_assert::irq_assert(bool manage_isr_service, gpio_num_t pin, gpio_int_type_t interrupt_type) : _pimpl{std::make_unique<impl>()} {
         if (manage_isr_service) {
             if (const auto res = gpio_install_isr_service(0); res != ESP_OK) {
@@ -45,7 +69,9 @@ namespace mlab {
     }
 
     bool irq_assert::operator()(ms timeout) {
-        if (not _pimpl->semaphore) {
+        if (_pimpl == nullptr) {
+            return true;
+        } else if (not _pimpl->semaphore) {
             ESP_LOGE("MLAB", "Attempt at taking invalid semaphore.");
             return false;
         }
@@ -53,19 +79,20 @@ namespace mlab {
     }
 
     irq_assert::~irq_assert() {
-        assert(_pimpl);
-        if (_pimpl->pin != GPIO_NUM_MAX) {
-            if (const auto res = gpio_isr_handler_remove(_pimpl->pin); res != ESP_OK) {
-                ESP_LOGW("MLAB", "gpio_isr_handler_remove failed with status %d (%s).", res, esp_err_to_name(res));
+        if (_pimpl != nullptr) {
+            if (_pimpl->pin != GPIO_NUM_MAX) {
+                if (const auto res = gpio_isr_handler_remove(_pimpl->pin); res != ESP_OK) {
+                    ESP_LOGW("MLAB", "gpio_isr_handler_remove failed with status %d (%s).", res, esp_err_to_name(res));
+                }
+                _pimpl->pin = GPIO_NUM_MAX;
             }
-            _pimpl->pin = GPIO_NUM_MAX;
-        }
-        if (_pimpl->semaphore) {
-            vPortFree(_pimpl->semaphore);
-            _pimpl->semaphore = nullptr;
-        }
-        if (_pimpl->manage_isr_service) {
-            gpio_uninstall_isr_service();
+            if (_pimpl->semaphore) {
+                vPortFree(_pimpl->semaphore);
+                _pimpl->semaphore = nullptr;
+            }
+            if (_pimpl->manage_isr_service) {
+                gpio_uninstall_isr_service();
+            }
         }
     }
 }

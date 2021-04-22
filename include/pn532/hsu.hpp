3772dca99a18160871b14a26f95d6f7a318c3c9c
Pietro Saccardi <lizardm4@gmail.com>
2021-04-22 18:36:19 +0200

    Docs: document HSU

--- include/pn532/hsu.hpp
+++ include/pn532/hsu.hpp
@@ -10,6 +10,9 @@
 
 namespace pn532 {
 
+    /**
+     * @brief Implementation of HSU channel protocol for PN532 over ESP32's I2C driver.
+     */
     class hsu_channel final : public channel {
         uart_port_t _port;
 
@@ -23,14 +26,18 @@ namespace pn532 {
 
     public:
         bool wake() override;
+
         /**
-         *
-         * @param port
-         * @param config
+         * @brief Construct an HSU channel for a PN532 with the given settings.
+         * @param port Communication port for the HSU channel. This is passed as-is to the UART driver.
+         * @param config Configuration for the HSU channel. This is passed as-is to the UART driver.
          * @param to_device_tx The pin connected to the TX line on the PN532
          * @param to_device_rx The pin connected to the RX line on the PN532
+         * @note In case of invalid port or configuration, an error message is printed, but the class is correctly constructed. It will simply
+         *  always fail to send and receive anything (and may clog your output with error messages).
          */
         hsu_channel(uart_port_t port, uart_config_t config, int to_device_tx, int to_device_rx);
+
         ~hsu_channel() override;
     };
 }// namespace pn532

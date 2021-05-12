3286b7fd8dfe0295bd4c446498c46547a26d55be
Pietro Saccardi <lizardm4@gmail.com>
2021-05-12 02:05:20 +0200

    Extend cipher provider to select the cipher class manually

--- include/desfire/cipher_provider.hpp
+++ include/desfire/cipher_provider.hpp
@@ -18,12 +18,18 @@ namespace desfire {
         virtual ~cipher_provider() = default;
     };
 
-    template <class CryptoDES, class Crypto2K3DES, class Crypto3K3DES, class CryptoAES>
+    template <class CryptoDES, class Crypto2K3DES, class Crypto3K3DES, class CryptoAES,
+              class CipherDES = cipher_legacy, class Cipher2K3DES = cipher_legacy,
+              class Cipher3K3DES = cipher_default, class CipherAES = cipher_default>
     struct typed_cipher_provider final : public cipher_provider {
-        static_assert(std::is_base_of_v<crypto_des_base, CryptoDES>);
-        static_assert(std::is_base_of_v<crypto_2k3des_base, Crypto2K3DES>);
-        static_assert(std::is_base_of_v<crypto_3k3des_base, Crypto3K3DES>);
-        static_assert(std::is_base_of_v<crypto_aes_base, CryptoAES>);
+        static_assert(std::is_base_of_v<crypto, CryptoDES>);
+        static_assert(std::is_base_of_v<crypto, Crypto2K3DES>);
+        static_assert(std::is_base_of_v<crypto, Crypto3K3DES>);
+        static_assert(std::is_base_of_v<crypto, CryptoAES>);
+        static_assert(std::is_base_of_v<cipher, CipherDES>);
+        static_assert(std::is_base_of_v<cipher, Cipher2K3DES>);
+        static_assert(std::is_base_of_v<cipher, Cipher3K3DES>);
+        static_assert(std::is_base_of_v<cipher, CipherAES>);
 
         [[nodiscard]] std::unique_ptr<cipher> setup_from_key(any_key const &key) override;
     };
@@ -31,28 +37,31 @@ namespace desfire {
 
 namespace desfire {
 
-    template <class CryptoDES, class Crypto2K3DES, class Crypto3K3DES, class CryptoAES>
-    std::unique_ptr<cipher> typed_cipher_provider<CryptoDES, Crypto2K3DES, Crypto3K3DES, CryptoAES>::setup_from_key(any_key const &key) {
+    template <class CryptoDES, class Crypto2K3DES, class Crypto3K3DES, class CryptoAES,
+            class CipherDES, class Cipher2K3DES, class Cipher3K3DES, class CipherAES>
+    std::unique_ptr<cipher> typed_cipher_provider<CryptoDES, Crypto2K3DES, Crypto3K3DES, CryptoAES,
+            CipherDES, Cipher2K3DES, Cipher3K3DES, CipherAES>::setup_from_key(any_key const &key)
+    {
         switch (key.type()) {
             case cipher_type::des: {
                 auto crypto = std::make_unique<CryptoDES>();
                 crypto->setup_with_key(make_range(key.template get<cipher_type::des>().k));
-                return std::make_unique<cipher_legacy>(std::move(crypto));
+                return std::make_unique<CipherDES>(std::move(crypto));
             }
             case cipher_type::des3_2k: {
                 auto crypto = std::make_unique<Crypto2K3DES>();
                 crypto->setup_with_key(make_range(key.template get<cipher_type::des3_2k>().k));
-                return std::make_unique<cipher_legacy>(std::move(crypto));
+                return std::make_unique<Cipher2K3DES>(std::move(crypto));
             }
             case cipher_type::des3_3k: {
                 auto crypto = std::make_unique<Crypto3K3DES>();
                 crypto->setup_with_key(make_range(key.template get<cipher_type::des3_3k>().k));
-                return std::make_unique<cipher_default>(std::move(crypto));
+                return std::make_unique<Cipher3K3DES>(std::move(crypto));
             }
             case cipher_type::aes128: {
                 auto crypto = std::make_unique<CryptoAES>();
                 crypto->setup_with_key(make_range(key.template get<cipher_type::aes128>().k));
-                return std::make_unique<cipher_default>(std::move(crypto));
+                return std::make_unique<CipherAES>(std::move(crypto));
             }
             case cipher_type::none:
                 [[fallthrough]];

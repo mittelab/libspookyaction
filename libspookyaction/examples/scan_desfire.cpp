133caab91bf5fd4131c0b2d3a9d66036cd71f873
Pietro Saccardi <lizardm4@gmail.com>
2021-11-30 02:59:43 +0100

    Update examples and fix examples CI

--- libspookyaction/examples/scan_desfire.cpp
+++ libspookyaction/examples/scan_desfire.cpp
@@ -5,12 +5,27 @@
 
 using namespace std::chrono_literals;
 
+/**
+ * @note This is the new function introduced in this example
+ */
+void scan_uuids(pn532::controller &pn532) {
+    if (auto res = pn532.initiator_list_passive_kbps106_typea(); res) {
+        for (pn532::target_kbps106_typea const &target : *res) {
+            ESP_LOGI(TAG, "Logical index %u; NFC ID:", target.logical_index);
+            ESP_LOG_BUFFER_HEX_LEVEL(TAG, target.info.nfcid.data(), target.info.nfcid.size(), ESP_LOG_INFO);
+        }
+    } else {
+        ESP_LOGE(TAG, "Failed to scan for passive targets at 106kbps (type A), error: %s", pn532::to_string(res.error()));
+    }
+}
+
+
+/**
+ * @note This is identical to the example in @ref initialize.cpp.
+ */
 extern "C" void app_main() {
-    // The GPIO pin number connected to the TX line on the PN532
     static constexpr gpio_num_t gpio_serial_tx = GPIO_NUM_17;
-    // The GPIO pin number connected to the RX line on the PN532
     static constexpr gpio_num_t gpio_serial_rx = GPIO_NUM_16;
-    // UART configuration. This is pretty much always the same
     static constexpr uart_config_t uart_config = {
             .baud_rate = 115200,
             .data_bits = UART_DATA_8_BITS,
@@ -19,27 +34,19 @@ extern "C" void app_main() {
             .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
             .rx_flow_ctrl_thresh = 122,
             .source_clk = UART_SCLK_REF_TICK};
-
-    // Create a communication channel (high speed UART, HSU) with the PN532.
     auto hsu_chn = pn532::esp32::hsu_channel(UART_NUM_1, uart_config, gpio_serial_tx, gpio_serial_rx);
-
-    // Create a new tag reader controller, i.e. a PN532 abstraction, that operates on top of the channel
     auto pn532 = pn532::controller(hsu_chn);
-
-    // Wake up the channel. This wakes the PN532 too.
     if (not hsu_chn.wake()) {
         ESP_LOGE(TAG, "HSU did not wake!");
         return;
     }
-
-    // Perform standard initialization, first initialize the Security Access Module, needed for everything
     if (not pn532.sam_configuration(pn532::sam_mode::normal, 1s)) {
         ESP_LOGE(TAG, "Failed to initialize SAM");
         return;
     }
-
-    // Now switch RF on, disable automatic field detection (used in target mode).
     if (not pn532.rf_configuration_field(false, true)) {
         ESP_LOGE(TAG, "Failed to switch RF field on");
+        return;
     }
+    scan_uuids(pn532);
 }
--- libspookyaction/examples/self_test.cpp
+++ libspookyaction/examples/self_test.cpp
@@ -1,21 +1,63 @@
-#include <driver/gpio.h>
-#include <driver/uart.h>
 #include <pn532/controller.hpp>
 #include <pn532/esp32/hsu.hpp>
 
-// PN532_SERIAL_TX: the GPIO pin number connected to the TX line on the PN532
-#ifndef PN532_SERIAL_TX
-#define PN532_SERIAL_TX (GPIO_NUM_17)
-#endif
-
-// PN532_SERIAL_RX: the GPIO pin number connected to the RX line on the PN532
-#ifndef PN532_SERIAL_RX
-#define PN532_SERIAL_RX (GPIO_NUM_16)
-#endif
+#define TAG "EXAMPLE"
 
 using namespace std::chrono_literals;
 
-std::pair<pn532::esp32::hsu_channel, pn532::controller> initialize_pn532() {
+const char *bool_to_ok_fail(bool result) {
+    return result ? "OK" : "FAIL";
+}
+
+const char *bool_to_yes_no(bool result) {
+    return result ? "YES" : "NO";
+}
+
+/**
+ * @note This is the new function introduced in this example
+ */
+void self_test(pn532::controller &pn532) {
+    // Autotest PN532 ROM firmware
+    ESP_LOGI(TAG, "ROM: %s", bool_to_ok_fail(bool(pn532.diagnose_rom())));
+
+    // Autotest PN532 RAM
+    ESP_LOGI(TAG, "RAM: %s", bool_to_ok_fail(bool(pn532.diagnose_ram())));
+
+    // Check card presence via ART or ISO/IEC14443-4 card presence detection
+    ESP_LOGI(TAG, "Card present: %s", bool_to_yes_no(bool(pn532.diagnose_attention_req_or_card_presence())));
+
+    // Test comunication line
+    ESP_LOGI(TAG, "Channel: %s", bool_to_ok_fail(bool(pn532.diagnose_comm_line())));
+
+    // Test target polling, this will search for FeliCa card with 212kbps or 424kbps baudrate, return number of failed attempt
+    ESP_LOGI(TAG, "Polling tag failures: ");
+    if (const auto poll_result = pn532.diagnose_poll_target(true, true); poll_result) {
+        ESP_LOGI(TAG, "   %d@212kbps %d@424kbps", poll_result->first, poll_result->second);
+    } else {
+        ESP_LOGI(TAG, "   Error: %s", pn532::to_string(poll_result.error()));
+    }
+
+    // Check antenna for open circuit, or short circuit
+    const auto antenna_test_result = pn532.diagnose_self_antenna(pn532::bits::low_current_thr::mA_25, pn532::bits::high_current_thr::mA_150);
+    ESP_LOGI(TAG, "Antenna: %s", bool_to_ok_fail(bool(antenna_test_result)));
+
+    // Get firmware version of the tag
+    ESP_LOGI(TAG, "PN532 info: ");
+    if (const auto fw_version_result = pn532.get_firmware_version(); fw_version_result) {
+        ESP_LOGI(TAG, "   IC: %#02x", fw_version_result->ic);
+        ESP_LOGI(TAG, "   Version: %#02x", fw_version_result->version);
+        ESP_LOGI(TAG, "   Revision: %#02x", fw_version_result->revision);
+    } else {
+        ESP_LOGI(TAG, "   Error: %s", pn532::to_string(fw_version_result.error()));
+    }
+}
+
+/**
+ * @note This is identical to the example in @ref initialize.cpp.
+ */
+extern "C" void app_main() {
+    static constexpr gpio_num_t gpio_serial_tx = GPIO_NUM_17;
+    static constexpr gpio_num_t gpio_serial_rx = GPIO_NUM_16;
     static constexpr uart_config_t uart_config = {
             .baud_rate = 115200,
             .data_bits = UART_DATA_8_BITS,
@@ -24,54 +66,19 @@ std::pair<pn532::esp32::hsu_channel, pn532::controller> initialize_pn532() {
             .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
             .rx_flow_ctrl_thresh = 122,
             .source_clk = UART_SCLK_REF_TICK};
-
-    auto hsu_chn = pn532::esp32::hsu_channel(UART_NUM_1, uart_config, PN532_SERIAL_TX, PN532_SERIAL_RX);
-    auto tag_reader = pn532::controller(hsu_chn);
-    hsu_chn.wake();
-    tag_reader.sam_configuration(pn532::sam_mode::normal, 1s);
-    tag_reader.rf_configuration_retries(pn532::infty);
-    // Switch on RF, disable auto field detection (used for card emulation)
-    tag_reader.rf_configuration_field(false, true);
-    return {std::move(hsu_chn), std::move(tag_reader)};
-}
-
-void self_test(pn532::controller &tag_reader) {
-    // Autotest PN532 ROM firmware
-    ESP_LOGI("EXAMPLE", "ROM: %s", tag_reader.diagnose_rom() ? "OK" : "FAIL");
-
-    // Autotest PN532 RAM
-    ESP_LOGI("EXAMPLE", "RAM: %s", tag_reader.diagnose_ram() ? "OK" : "FAIL");
-
-    // Check card presence via ART or ISO/IEC14443-4 card presence detection
-    ESP_LOGI("EXAMPLE", "CARD PRESENT: %s", tag_reader.diagnose_attention_req_or_card_presence() ? "YES" : "NO");
-
-    // Test comunication line
-    ESP_LOGI("EXAMPLE", "COMUNICATION: %s", tag_reader.diagnose_comm_line() ? "OK" : "FAIL");
-
-    // Test target polling, this will search for FeliCa card with 212kbps or 424kbps baudrate, return number of failed attempt
-    ESP_LOGI("EXAMPLE", "POLL TAG: ");
-    if (const auto poll_result = tag_reader.diagnose_poll_target(true, true); poll_result) {
-        ESP_LOGI("EXAMPLE", "%d@212kbps %d@424kbps", poll_result->first, poll_result->second);
-    } else {
-        ESP_LOGI("EXAMPLE", "ERROR (%s)", pn532::to_string(poll_result.error()));
+    auto hsu_chn = pn532::esp32::hsu_channel(UART_NUM_1, uart_config, gpio_serial_tx, gpio_serial_rx);
+    auto pn532 = pn532::controller(hsu_chn);
+    if (not hsu_chn.wake()) {
+        ESP_LOGE(TAG, "HSU did not wake!");
+        return;
     }
-
-    // Check antenna for open circuit, or short circuit
-    const auto antenna_test_result = tag_reader.diagnose_self_antenna(pn532::bits::low_current_thr::mA_25, pn532::bits::high_current_thr::mA_150);
-    ESP_LOGI("EXAMPLE", "Antenna: %s", antenna_test_result ? "OK" : "ERROR");
-
-    // Get firmware version of the tag
-    ESP_LOGI("EXAMPLE", "PN532: ");
-    if (const auto fw_version_result = tag_reader.get_firmware_version(); fw_version_result) {
-        ESP_LOGI("EXAMPLE", "IC: %#02x", fw_version_result->ic);
-        ESP_LOGI("EXAMPLE", "version: %#02x", fw_version_result->version);
-        ESP_LOGI("EXAMPLE", "revision: %#02x", fw_version_result->revision);
-    } else {
-        ESP_LOGI("EXAMPLE", "ERROR (%s)", pn532::to_string(fw_version_result.error()));
+    if (not pn532.sam_configuration(pn532::sam_mode::normal, 1s)) {
+        ESP_LOGE(TAG, "Failed to initialize SAM");
+        return;
     }
-}
-
-extern "C" void app_main() {
-    auto [hsu_chn, tag_reader] = initialize_PN532();
-    self_test(tag_reader);
+    if (not pn532.rf_configuration_field(false, true)) {
+        ESP_LOGE(TAG, "Failed to switch RF field on");
+        return;
+    }
+    self_test(pn532);
 }

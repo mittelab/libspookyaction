78beaea5c65bebd623940b3da66420e53f53d7bf
Pietro Saccardi <lizardm4@gmail.com>
2022-09-14 19:47:34 +0200

    Revert "Separate cmac base functionality for KDF"
    This reverts commit 99ae4b8f5f26dfdaf4938f7a34f79122ed2ff44e.

--- libspookyaction/src/desfire/cmac_provider.cpp
+++ libspookyaction/src/desfire/cmac_provider.cpp
@@ -9,7 +9,7 @@
 
 namespace desfire {
 
-    void cmac_provider_base::prepare_subkey(range<std::uint8_t *> subkey, std::uint8_t last_byte_xor) {
+    void cmac_provider::prepare_subkey(range<std::uint8_t *> subkey, std::uint8_t last_byte_xor) {
         const bool do_xor = (*std::begin(subkey) & 0x80) != 0;
         // Some app-specific magic: lshift by one
         lshift_sequence(std::begin(subkey), std::end(subkey), 1);
@@ -19,7 +19,7 @@ namespace desfire {
         }
     }
 
-    void cmac_provider_base::initialize_subkeys() {
+    void cmac_provider::initialize_subkeys() {
         auto rg_key_nopad = key_nopad();
         auto rg_key_pad = key_pad();
 
@@ -45,43 +45,38 @@ namespace desfire {
         ESP_LOG_BUFFER_HEX_LEVEL(DESFIRE_TAG " KEY", _subkey_pad.get(), block_size(), ESP_LOG_DEBUG);
     }
 
-    void cmac_provider_base::do_cmac_crypto(mlab::bin_data &buffer, range<std::uint8_t *> iv, range<const std::uint8_t *> data) {
+
+    cmac_provider::mac_t cmac_provider::compute_cmac(range<std::uint8_t *> iv, range<std::uint8_t const *> data) {
+        mac_t retval{0, 0, 0, 0, 0, 0, 0, 0};
+
         if (iv.size() < block_size()) {
             DESFIRE_LOGE("CMAC: got %d bytes for IV, need at least %d for CMAC.", iv.size(), block_size());
+            return retval;
         }
 
         static const auto xor_op = [](std::uint8_t l, std::uint8_t r) -> std::uint8_t { return l ^ r; };
 
         // Resize the buffer and copy data
-        buffer.clear();
-        buffer.resize(padded_length(data.size(), block_size()));
+        _cmac_buffer.clear();
+        _cmac_buffer.resize(padded_length(data.size(), block_size()));
 
-        std::copy(std::begin(data), std::end(data), std::begin(buffer));
+        std::copy(std::begin(data), std::end(data), std::begin(_cmac_buffer));
 
         // Spec requires XOR-ing the last block with the appropriate key.
-        const auto last_block = buffer.view(buffer.size() - block_size());
-        if (buffer.size() == data.size()) {
+        const auto last_block = _cmac_buffer.view(_cmac_buffer.size() - block_size());
+        if (_cmac_buffer.size() == data.size()) {
             // Was not padded
-            std::transform(std::begin(last_block), std::end(last_block), key_nopad().begin(),
+            std::transform(std::begin(last_block), std::end(last_block), _subkey_nopad.get(),
                            std::begin(last_block), xor_op);
         } else {
             // Was padded, but spec wants to pad with 80 00 .. 00, so change one byte
-            buffer[data.size()] = 0x80;
-            std::transform(std::begin(last_block), std::end(last_block), key_pad().begin(),
+            _cmac_buffer[data.size()] = 0x80;
+            std::transform(std::begin(last_block), std::end(last_block), _subkey_pad.get(),
                            std::begin(last_block), xor_op);
         }
 
         // Return the first 8 bytes of the last block
-        crypto_implementation().do_crypto(buffer.data_view(), iv, crypto_operation::mac);
-    }
-
-
-    cmac_provider::mac_t cmac_provider::compute_cmac(range<std::uint8_t *> iv, range<std::uint8_t const *> data) {
-        mac_t retval{0, 0, 0, 0, 0, 0, 0, 0};
-
-        // Return the first 8 bytes of the last block
-        do_cmac_crypto(_cmac_buffer, iv, data);
-
+        crypto_implementation().do_crypto(_cmac_buffer.data_view(), iv, crypto_operation::mac);
         std::copy(std::begin(iv), std::begin(iv) + retval.size(), std::begin(retval));
         return retval;
     }

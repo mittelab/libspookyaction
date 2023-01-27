05ab22121b503a33bf82456b4b0710b7b04cdbb3
Pietro Saccardi <lizardm4@gmail.com>
2023-01-27 23:02:42 +0100

    Give ability to print fail msg without failing

--- libspookyaction/include/desfire/fs.hpp
+++ libspookyaction/include/desfire/fs.hpp
@@ -14,9 +14,12 @@
 #define DESFIRE_FS_LOG_PREFIX DESFIRE_FS_DEFAULT_LOG_PREFIX
 #endif
 
-#define DESFIRE_FAIL_CMD(CMD_STR, RESULT)                               \
+#define DESFIRE_FAIL_MSG(CMD_STR, RESULT)                               \
     ESP_LOGW(DESFIRE_FS_LOG_PREFIX, "%s:%d failed " CMD_STR " with %s", \
-             __FILE__, __LINE__, to_string(RESULT.error()));            \
+             __FILE__, __LINE__, to_string(RESULT.error()));
+
+#define DESFIRE_FAIL_CMD(CMD_STR, RESULT) \
+    DESFIRE_FAIL_MSG(CMD_STR, RESULT)     \
     return RESULT.error();
 
 #define DESFIRE_CMD_WITH_NAMED_RESULT(CMD, RESULT_NAME) \

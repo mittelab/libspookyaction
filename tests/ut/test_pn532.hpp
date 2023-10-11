97f9d0cc0bd37f57903b8c3d2befcbe038df6303
Pietro Saccardi <lizardm4@gmail.com>
2023-10-11 21:53:08 +0200

    Fix powering down preventing further wake
    [skip ci]

--- tests/ut/test_pn532.hpp
+++ tests/ut/test_pn532.hpp
@@ -10,6 +10,7 @@
 #include <catch.hpp>
 #include <memory>
 #include <pn532/controller.hpp>
+#include <thread>
 
 namespace ut::pn532 {
     using namespace ::pn532;
@@ -20,6 +21,17 @@ namespace ut::pn532 {
     struct channel_fixture {
         std::unique_ptr<channel> chn = try_activate_channel(CT);
         std::unique_ptr<controller> ctrl = chn ? std::make_unique<controller>(*chn) : nullptr;
+
+        /**
+         * @note We must power down the PN532 if we want to launch sam_configuration at every test case.
+         * When powering down, also ensure to wait some time otherwise it might miss the next wake call.
+         */
+        ~channel_fixture() {
+            if (ctrl) {
+                ctrl->power_down({wakeup_source::hsu, wakeup_source::spi, wakeup_source::i2c});
+                std::this_thread::sleep_for(50ms);
+            }
+        }
 
         [[nodiscard]] virtual inline explicit operator bool() const { return chn and ctrl; }
     };

d77bebc4994951ff27d19d2fe9c740c14d23d70a
Pietro Saccardi <lizardm4@gmail.com>
2023-10-17 16:18:37 +0200

    Convert to Catch

--- tests/ut/test_desfire_files.cpp
+++ tests/ut/test_desfire_files.cpp
@@ -2,239 +2,145 @@
 // Created by spak on 3/23/21.
 //
 
-#include "test_desfire_files.hpp"
-#include "utils.hpp"
+#include "test_desfire_main.hpp"
+#include <catch.hpp>
 #include <numeric>
-#include <unity.h>
 
 namespace ut::desfire_files {
-    using namespace mlab_literals;
+    using namespace ut::desfire;
+    using namespace ut::pn532;
 
-    static constexpr const char *missing_instance_msg = "File test instance was not set up.";
-
-    test_data::test_data(std::shared_ptr<ut::desfire_main::test_instance> main_test_instance)
-        : _hold_test_instance{std::move(main_test_instance)}, _file{}, _test_load{} {
-        _test_load.resize(0x100);
-        std::iota(std::begin(_test_load), std::end(_test_load), 0x00);
-    }
-
-    tag &test_data::tag() {
-        return _hold_test_instance->tag();
-    }
-
-    demo_file &test_data::file() {
-        return _file;
-    }
-
-    bin_data &test_data::test_load() {
-        return _test_load;
-    }
-
-
-    void demo_file::delete_if_preexisting(tag &tag) const {
-        const auto r_get_fids = tag.get_file_ids();
-        TEST_ASSERT(r_get_fids);
-        if (std::find(std::begin(*r_get_fids), std::end(*r_get_fids), fid()) != std::end(*r_get_fids)) {
-            TEST_ASSERT(tag.abort_transaction());
-            TEST_ASSERT(tag.delete_file(fid()));
+    TEMPLATE_TEST_CASE_METHOD_SIG(card_fixture, "0040 Desfire files", "",
+                                  ((channel_type CT), CT),
+                                  channel_type::hsu, channel_type::i2c, channel_type::i2c_irq, channel_type::spi, channel_type::spi_irq) {
+        if (not channel_is_supported(CT)) {
+            SKIP("Unsupported channel type " << ut::pn532::to_string(CT));
         }
-    }
 
-    const char *demo_file::security_description() const {
-        switch (security) {
-            case file_security::none:
-                return "none";
-            case file_security::encrypted:
-                return "encrypted";
-            case file_security::authenticated:
-                return "maced";
-        }
-        return nullptr;
-    }
+        REQUIRE(*this);
 
-    const char *demo_file::cipher_description() const {
-        switch (cipher) {
-            case cipher_type::des:
-                return "des";
-            case cipher_type::des3_2k:
-                return "des3_2k";
-            case cipher_type::des3_3k:
-                return "des3_3k";
-            case cipher_type::aes128:
-                return "aes128";
-            case cipher_type::none:
-                break;
-        }
-        return nullptr;
-    }
+        // Create a temp app which will auto-delete
+        const auto cipher = GENERATE(cipher_type::des, cipher_type::des3_2k, cipher_type::des3_3k, cipher_type::aes128);
+        app_fixture_setup fmt{*this->mifare, cipher};
 
-    const char *demo_file::type_description() const {
-        switch (type) {
-            case file_type::standard:
-                return "standard";
-            case file_type::backup:
-                return "backup";
-            case file_type::value:
-                return "value";
-            case file_type::linear_record:
-                return "linear_record";
-            case file_type::cyclic_record:
-                return "cyclic_record";
-        }
-        return nullptr;
-    }
+        fmt.select_and_authenticate();
 
-    std::string demo_file::get_description() const {
-        std::string buffer;
-        buffer.reserve(128);
-        // Here the buffer get cleared
-        buffer.append("ut::desfire_files::test_file {.cipher=");
-        buffer.append(cipher_description());
-        buffer.append(", .type=");
-        buffer.append(type_description());
-        buffer.append(", .security=");
-        buffer.append(security_description());
-        buffer.append(free_access ? ", .free=true}" : ", .free=false}");
-        return buffer;
-    }
+        const auto security = GENERATE(file_security::none, file_security::authenticated, file_security::encrypted);
+        const auto free_access = GENERATE(true, false);
 
-    file_id demo_file::fid() {
-        return 0x00;
-    }
-
-    any_file_settings demo_file::get_settings() const {
+        static constexpr file_id fid = 0x00;
         static constexpr data_file_settings dfs{0x100};
         static constexpr record_file_settings rfs{8, 2};
         static constexpr value_file_settings vfs{-10, 10, 0, true};
-        // Select either all keys, or one key (the one we are using
+
+        mlab::bin_data test_payload;
+        test_payload.resize(0x100);
+        std::iota(std::begin(test_payload), std::end(test_payload), 0x00);
+
+        // Select either all keys, or one key (the one we are using)
         const common_file_settings gfs{security, free_access ? file_access_rights{desfire::free_access} : file_access_rights{0_b}};
 
-        switch (type) {
-            case file_type::standard:
-                return file_settings<file_type::standard>{gfs, dfs};
-            case file_type::backup:
-                return file_settings<file_type::backup>{gfs, dfs};
-            case file_type::value:
-                return file_settings<file_type::value>{gfs, vfs};
-            case file_type::linear_record:
-                return file_settings<file_type::linear_record>{gfs, rfs};
-            case file_type::cyclic_record:
-                return file_settings<file_type::cyclic_record>{gfs, rfs};
-            default:
-                ESP_LOGE("UT", "Unknown file type %s.", to_string(type));
-                return {};
+        SECTION("Standard") {
+            REQUIRE(this->mifare->create_file(fid, file_settings<file_type::standard>{gfs, dfs}));
+            REQUIRE(this->mifare->write_data(fid, test_payload, trust_card));
+            const auto r_read = this->mifare->read_data(fid, trust_card, 0, test_payload.size());
+            CHECKED_IF(r_read) {
+                CHECK(test_payload == *r_read);
+            }
         }
-    }
 
-    void demo_file::test(tag &mifare, bin_data const &test_load) {
-        const any_key root_key{key<cipher_type::des>{}};
+        SECTION("Backup") {
+            REQUIRE(this->mifare->create_file(fid, file_settings<file_type::backup>{gfs, dfs}));
+            REQUIRE(this->mifare->write_data(fid, test_payload, trust_card));
 
-        // Make sure there is enough space to run. 1376B is a decent estimate for how much space is needed
-        TEST_ASSERT(mifare.select_application(root_app));
-        TEST_ASSERT(mifare.authenticate(root_key));
-        const auto r_free_mem = mifare.get_free_mem();
-        TEST_ASSERT(r_free_mem);
-        if (*r_free_mem < 1376) {
-            ESP_LOGI("UT", "Formatting to recover space (only %lu B free).", *r_free_mem);
-            TEST_ASSERT(mifare.format_picc());
+            const auto r_read_before_commit = this->mifare->read_data(fid, trust_card, 0, test_payload.size());
+            CHECKED_IF(r_read_before_commit) {
+                CHECK(r_read_before_commit->size() == test_payload.size());
+                const auto all_zero = std::all_of(std::begin(*r_read_before_commit), std::end(*r_read_before_commit), [](auto b) { return b == 0; });
+                CHECK(all_zero);
+            }
+
+            REQUIRE(this->mifare->commit_transaction());
+            const auto r_read = this->mifare->read_data(fid, trust_card, 0, test_payload.size());
+            CHECKED_IF(r_read) {
+                CHECK(*r_read == test_payload);
+            }
         }
-        const ut::desfire_main::demo_app app{cipher};
-        app.ensure_created(mifare, root_key);
-        app.ensure_selected_and_primary(mifare);
-        TEST_ASSERT_EQUAL_HEX8_ARRAY(app.aid.data(), mifare.active_app().data(), 3);
-        TEST_ASSERT_EQUAL(app.primary_key.key_number(), mifare.active_key_no());
-        delete_if_preexisting(mifare);
-        TEST_ASSERT(mifare.create_file(fid(), get_settings()));
 
-        switch (type) {
-            case file_type::standard:
-                test_standard_data_file(mifare, test_load);
-                break;
-            case file_type::backup:
-                test_backup_data_file(mifare, test_load);
-                break;
-            case file_type::value:
-                test_value_file(mifare);
-                break;
-            case file_type::linear_record:
-                [[fallthrough]];
-            case file_type::cyclic_record:
-                test_record_file(mifare);
-                break;
+        SECTION("Value") {
+            REQUIRE(this->mifare->create_file(fid, file_settings<file_type::value>{gfs, vfs}));
+
+            auto res_read = this->mifare->get_value(fid, trust_card);
+            CHECKED_IF(res_read) {
+                CHECK(*res_read == 0);
+            }
+
+            REQUIRE(this->mifare->credit(fid, 2, trust_card));
+
+            res_read = this->mifare->get_value(fid, trust_card);
+            CHECKED_IF(res_read) {
+                CHECK(*res_read == 0);// Did not commit yet
+            }
+
+            REQUIRE(this->mifare->commit_transaction());
+
+            res_read = this->mifare->get_value(fid, trust_card);
+            CHECKED_IF(res_read) {
+                CHECK(*res_read == 2);
+            }
+
+            REQUIRE(this->mifare->debit(fid, 5, trust_card));
+            REQUIRE(this->mifare->commit_transaction());
+
+            res_read = this->mifare->get_value(fid, trust_card);
+            CHECKED_IF(res_read) {
+                CHECK(*res_read == -3);
+            }
         }
-        TEST_ASSERT(mifare.change_file_settings(fid(), get_settings().common_settings(), trust_card));
-        TEST_ASSERT(mifare.delete_file(fid()));
-    }
 
-    void demo_file::test_standard_data_file(tag &mifare, bin_data const &load) const {
-        TEST_ASSERT(mifare.write_data(fid(), load, trust_card));
-        const auto r_read = mifare.read_data(fid(), trust_card, 0, load.size());
-        TEST_ASSERT(r_read);
-        TEST_ASSERT_EQUAL(load.size(), r_read->size());
-        TEST_ASSERT_EQUAL_HEX8_ARRAY(load.data(), r_read->data(), load.size());
-    }
+        SECTION("Record") {
+            using record_t = std::array<std::uint8_t, 8>;
+            const mlab::bin_data nibble = {0x00, 0x01, 0x02, 0x03};
 
-    void demo_file::test_backup_data_file(tag &mifare, bin_data const &load) const {
-        TEST_ASSERT(mifare.write_data(fid(), load, trust_card));
-        const auto r_read_before_commit = mifare.read_data(fid(), trust_card, 0, load.size());
-        TEST_ASSERT(r_read_before_commit);
-        TEST_ASSERT_EACH_EQUAL_HEX8(0x00, r_read_before_commit->data(), r_read_before_commit->size());
-        TEST_ASSERT(mifare.commit_transaction());
-        const auto r_read = mifare.read_data(fid(), trust_card, 0, load.size());
-        TEST_ASSERT(r_read);
-        TEST_ASSERT_EQUAL(load.size(), r_read->size());
-        TEST_ASSERT_EQUAL_HEX8_ARRAY(load.data(), r_read->data(), load.size());
-    }
+            SECTION("Linear") {
+                REQUIRE(this->mifare->create_file(fid, file_settings<file_type::linear_record>{gfs, rfs}));
+            }
 
-    void demo_file::test_value_file(tag &mifare) const {
-        const auto test_get_value = [&](std::int32_t expected) {
-            const auto res_read = mifare.get_value(fid(), trust_card);
-            TEST_ASSERT(res_read);
-            TEST_ASSERT_EQUAL(expected, *res_read);
-        };
+            SECTION("Cyclic") {
+                REQUIRE(this->mifare->create_file(fid, file_settings<file_type::cyclic_record>{gfs, rfs}));
+            }
 
-        test_get_value(0);
-        TEST_ASSERT(mifare.credit(fid(), 2, trust_card));
-        test_get_value(0);// Did not commit yet
-        TEST_ASSERT(mifare.commit_transaction());
-        test_get_value(2);
-        TEST_ASSERT(mifare.debit(fid(), 5, trust_card));
-        TEST_ASSERT(mifare.commit_transaction());
-        test_get_value(-3);
-    }
+            auto r_settings = this->mifare->get_file_settings(fid);
+            CHECKED_IF(r_settings) {
+                CHECK(r_settings->record_settings().record_count == 0);
+            }
 
-    void demo_file::test_record_file(tag &mifare) const {
-        using record_t = std::array<std::uint8_t, 8>;
+            REQUIRE(this->mifare->write_record(fid, nibble, trust_card, 4));
+            REQUIRE(this->mifare->commit_transaction());
 
-        const mlab::bin_data nibble = {0x00, 0x01, 0x02, 0x03};
+            r_settings = this->mifare->get_file_settings(fid);
+            CHECKED_IF(r_settings) {
+                CHECK(r_settings->record_settings().record_count == 1);
+            }
 
-        const auto test_get_record_count = [&](std::uint32_t expected) {
-            const auto res_settings = mifare.get_file_settings(fid());
-            TEST_ASSERT(res_settings);
-            TEST_ASSERT_EQUAL(expected, res_settings->record_settings().record_count);
-        };
+            const auto r_records = this->mifare->template read_parse_records<record_t>(fid, trust_card, 0, all_records);
 
-        test_get_record_count(0);
-        TEST_ASSERT(mifare.write_record(fid(), nibble, trust_card, 4));
-        TEST_ASSERT(mifare.commit_transaction());
-        test_get_record_count(1);
-        const auto res_records = mifare.read_parse_records<record_t>(fid(), trust_card, 0, all_records);
-        TEST_ASSERT(res_records);
-        TEST_ASSERT_EQUAL(res_records->size(), 1);
-        const record_t expected = {0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03};
-        TEST_ASSERT_EQUAL_HEX8_ARRAY(expected.data(), res_records->front().data(), 8);
-        TEST_ASSERT(mifare.clear_record_file(fid()));
-        TEST_ASSERT(mifare.commit_transaction());
-    }
+            CHECKED_IF(r_records) {
+                CHECK(r_records->size() == 1);
+                CHECK(r_records->front() == record_t{0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03});
+            }
 
-    void test_file() {
-        UNITY_PATCH_TEST_FILE;
-        auto instance = default_registrar().get<test_instance>();
-        if (instance == nullptr) {
-            TEST_FAIL_MESSAGE(missing_instance_msg);
-            return;
+            REQUIRE(this->mifare->clear_record_file(fid));
+            REQUIRE(this->mifare->commit_transaction());
+
+            r_settings = this->mifare->get_file_settings(fid);
+            CHECKED_IF(r_settings) {
+                CHECK(r_settings->record_settings().record_count == 0);
+            }
         }
-        instance->file().test(instance->tag(), instance->test_load());
+
+        REQUIRE(this->mifare->change_file_settings(fid, gfs, trust_card));
+        REQUIRE(this->mifare->delete_file(fid));
     }
 
 }// namespace ut::desfire_files

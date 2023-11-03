9a3c1c2984f21fe9d6536e1c626627305b03826f
Pietro Saccardi <lizardm4@gmail.com>
2023-11-03 19:13:54 +0100

    Add app name
    [skip ci]

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,7 @@
 
 extern "C" int app_main() {
     Catch::Session session;
+    session.configData().name = "libSpookyAction";
     session.configData().runOrder = Catch::TestRunOrder::LexicographicallySorted;
     session.configData().verbosity = Catch::Verbosity::Quiet;
     session.configData().noThrow = true;

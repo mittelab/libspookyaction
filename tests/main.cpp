2aea4af2b1a64461b0c72da7c00809bde2509bf5
Pietro Saccardi <lizardm4@gmail.com>
2023-11-03 18:14:54 +0100

    Attempt changing reporter style to sections

--- tests/main.cpp
+++ tests/main.cpp
@@ -14,17 +14,82 @@ namespace {
     constexpr auto ansi_cyn [[maybe_unused]] = "\33[0;36m";
     constexpr auto ansi_wht [[maybe_unused]] = "\33[0;37m";
 
+    unsigned _vprintf_indent_amount = 0;
+
     extern "C" int vprintf_indent(const char *fmt, va_list argp) {
-        std::string fmt_s = fmt;
-        fmt_s = ".... " + fmt_s;
+        std::string fmt_s(std::min(_vprintf_indent_amount, 50u) * 2, '.');
+        fmt_s += fmt;
         return std::vprintf(fmt_s.c_str(), argp);
     }
 
     class SpookyReporter : public Catch::StreamingReporterBase {
+        struct section {
+            std::string full_name;
+            Catch::Counts counts;
+        };
+        std::vector<section> _stack;
+        std::map<std::string, Catch::Counts> _all_sections;
+
         vprintf_like_t _orig_vprintf = nullptr;
-        std::string _sep = std::string(60llu, '-');
-        std::vector<std::pair<std::string, Catch::ResultWas::OfType>> _tests;
-        std::vector<std::string> _sections;
+
+        void push_section(std::string name) {
+            if (not _stack.empty()) {
+                name = mlab::concatenate({_stack.back().full_name, "/", name});
+            }
+            _stack.emplace_back(section{std::move(name), {}});
+            _vprintf_indent_amount += 1;
+        }
+
+        [[nodiscard]] section pop_section(Catch::Counts counts) {
+            // Save the counts for this section
+            _stack.back().counts += counts;
+            // As this section ends, make sure you have saved (by name) the outcome
+            if (auto it = _all_sections.try_emplace(_stack.back().full_name).first; it != std::end(_all_sections)) {
+                it->second += counts;
+            }
+            // Also add the total assertions into the parent.
+            section retval = std::move(_stack.back());
+            _stack.pop_back();
+            _stack.back().counts += counts;
+            _vprintf_indent_amount -= 1;
+            return retval;
+        }
+
+        [[nodiscard]] static std::string get_indent_str(char c = '>', int delta = 0) {
+            std::string indent(std::max(0, int(_vprintf_indent_amount) + delta) * 2, '>');
+            if (not indent.empty()) {
+                indent += " ";
+            }
+            return indent;
+        }
+
+        static void print_starting_header(std::string const &name, const char *slug) {
+            const auto indent = get_indent_str('>', -1);
+            std::printf("%s%s%s %s%s %s\n", indent.c_str(), ansi_cyn, slug, "START", ansi_rst, name.c_str());
+        }
+
+        [[nodiscard]] static std::pair<const char *, const char *> get_status_and_ansi_color(Catch::Counts const &counts) {
+            if (counts.failed > 0) {
+                return {"FAIL", ansi_red};
+            } else if (counts.skipped > 0) {
+                return {"SKIP", ansi_mag};
+            }
+            return {"PASS", ansi_grn};
+        }
+
+        static void print_ending_header(std::string const &name, const char *slug, Catch::Counts const &counts) {
+            const auto indent = get_indent_str();
+            auto [log_str, log_str_col] = get_status_and_ansi_color(counts);
+
+            std::printf("%s%s%s%s  %s%s%s %s\n", indent.c_str(), ansi_cyn, slug, ansi_rst, log_str_col, log_str, ansi_rst, name.c_str());
+            std::printf("%s      %s%s%s %s%llu/%llu%s\n", indent.c_str(), ansi_cyn, "WITH", ansi_rst, log_str_col,
+                        counts.passed, counts.total(), ansi_rst);
+        }
+
+        [[nodiscard]] std::string const &current_section_name() const {
+            static const std::string _no_s = "<no section>";
+            return _stack.empty() ? _no_s : _stack.back().full_name;
+        }
     public:
         using Catch::StreamingReporterBase::StreamingReporterBase;
 
@@ -33,35 +98,25 @@ namespace {
         }
 
         void testRunStarting(const Catch::TestRunInfo &testRunInfo) override {
-            _orig_vprintf = esp_log_set_vprintf(&vprintf_indent);
-            std::printf("%s\n\n", _sep.c_str());
+//            _orig_vprintf = esp_log_set_vprintf(&vprintf_indent);
+            std::printf("\n");
             StreamingReporterBase::testRunStarting(testRunInfo);
         }
 
         void testRunEnded(const Catch::TestRunStats &testRunInfo) override {
-            esp_log_set_vprintf(_orig_vprintf);
+//            esp_log_set_vprintf(_orig_vprintf);
+            const std::string sep(60, '-');
 
-            std::printf("%s\n\n", _sep.c_str());
+            std::printf("%s\n\n", sep.c_str());
             std::printf("%sSUMMARY%s\n\n", ansi_cyn, ansi_rst);
 
-            for (std::size_t i = 0; i < _tests.size(); ++i) {
-                auto const &[name, result_was] = _tests[i];
-                switch (result_was) {
-                    case Catch::ResultWas::Ok:
-                        std::printf("%5d. %s%s%s %s\n", i + 1, ansi_grn, "PASS", ansi_rst, name.c_str());
-                        break;
-                    case Catch::ResultWas::ExplicitSkip:
-                        std::printf("%5d. %s%s%s %s\n", i + 1, ansi_mag, "SKIP", ansi_rst, name.c_str());
-                        break;
-                    case Catch::ResultWas::ExplicitFailure:
-                        std::printf("%5d. %s%s%s %s\n", i + 1, ansi_red, "FAIL", ansi_rst, name.c_str());
-                        break;
-                    default:
-                        ESP_LOGE("CATCH", "Invalid result type.");
-                        break;
-                }
+            std::size_t i = 0;
+            for (auto const &[name, counts] : _all_sections) {
+                auto [log_str, log_str_col] = get_status_and_ansi_color(counts);
+                std::printf("%5d. %s%s%s %s\n", ++i, log_str_col, log_str, ansi_rst, name.c_str());
             }
-            std::printf("\n%s\n\n", _sep.c_str());
+
+            std::printf("\n%s\n\n", sep.c_str());
 
             auto test_pass_color = testRunInfo.totals.testCases.failed == 0 ? ansi_grn : ansi_rst;
             auto test_fail_color = testRunInfo.totals.testCases.failed == 0 ? ansi_rst : ansi_red;
@@ -104,55 +159,14 @@ namespace {
 
         void sectionStarting(const Catch::SectionInfo &sectionInfo) override {
             StreamingReporterBase::sectionStarting(sectionInfo);
-            _sections.emplace_back(sectionInfo.name);
-            if (_sections.size() > 1) {
-                const auto full_section_name = mlab::concatenate_s(_sections, " > ");
-                std::printf("%sSECT START%s %s\n", ansi_blu, ansi_rst, full_section_name.c_str());
-            }
+            push_section(sectionInfo.name);
+            print_starting_header(sectionInfo.name, "SECT");
         }
 
         void sectionEnded(const Catch::SectionStats &sectionInfo) override {
+            auto s = pop_section(sectionInfo.assertions);
+            print_ending_header(sectionInfo.sectionInfo.name, "SECT", s.counts);
             StreamingReporterBase::sectionEnded(sectionInfo);
-            if (_sections.size() > 1) {
-                std::printf("%s\n", _sep.c_str());
-            }
-            _sections.pop_back();
-        }
-
-        void testCaseStarting(const Catch::TestCaseInfo &testInfo) override {
-            StreamingReporterBase::testCaseStarting(testInfo);
-            std::printf("%sTEST START%s %s\n", ansi_cyn, ansi_rst, testInfo.name.c_str());
-        }
-
-        void testCaseEnded(const Catch::TestCaseStats &stats) override {
-            const char *log_str = "PASS";
-            const char *log_str_col = ansi_grn;
-
-            if (stats.totals.testCases.failed > 0) {
-                log_str = "FAIL";
-                log_str_col = ansi_red;
-                _tests.emplace_back(stats.testInfo->name, Catch::ResultWas::OfType::ExplicitFailure);
-            } else if (stats.totals.testCases.skipped > 0) {
-                log_str = "SKIP";
-                log_str_col = ansi_mag;
-                _tests.emplace_back(stats.testInfo->name, Catch::ResultWas::OfType::ExplicitSkip);
-            } else {
-                _tests.emplace_back(stats.testInfo->name, Catch::ResultWas::OfType::Ok);
-            }
-
-            std::printf("%sTEST%s %s%s%s  %s\n",
-                        ansi_cyn, ansi_rst,
-                        log_str_col, log_str, ansi_rst, stats.testInfo->name.c_str());
-
-            if (stats.totals.testCases.skipped == 0) {
-                std::printf("%sWITH%s       %llu/%llu assertions\n",
-                            ansi_cyn, ansi_rst,
-                            stats.totals.assertions.passed,
-                            stats.totals.assertions.total());
-            }
-
-            std::printf("\n%s\n\n", _sep.c_str());
-            StreamingReporterBase::testCaseEnded(stats);
         }
 
         void assertionEnded(const Catch::AssertionStats &stats) override {
@@ -164,31 +178,32 @@ namespace {
             {
                 return;
             }
+            const auto indent = get_indent_str();
             switch (result.getResultType()) {
                 case Catch::ResultWas::Info:
                     if (result.hasMessage()) {
-                        std::printf(">>>> %sINFO%s  %s\n", ansi_cyn, ansi_rst, result.getMessage().data());
+                        std::printf("%s%sINFO%s  %s\n", indent.c_str(), ansi_cyn, ansi_rst, result.getMessage().data());
                     }
                     break;
                 case Catch::ResultWas::Warning:
                     if (result.hasMessage()) {
-                        std::printf(">>>> %sWARN%s  %s\n", ansi_yel, ansi_rst, result.getMessage().data());
+                        std::printf("%s%sWARN%s  %s\n", indent.c_str(), ansi_yel, ansi_rst, result.getMessage().data());
                     }
                     break;
                 case Catch::ResultWas::ExplicitSkip:
                     if (result.hasMessage()) {
-                        std::printf(">>>> %sSKIP%s  %s\n", ansi_mag, ansi_rst, result.getMessage().data());
+                        std::printf("%s%sSKIP%s  %s\n", indent.c_str(), ansi_mag, ansi_rst, result.getMessage().data());
                     } else if (result.hasExpression()) {
-                        std::printf(">>>> %sSKIP%s  %s\n", ansi_mag, ansi_rst, result.getExpressionInMacro().data());
+                        std::printf("%s%sSKIP%s  %s\n", indent.c_str(), ansi_mag, ansi_rst, result.getExpressionInMacro().data());
                     }
                     break;
                 default:
-                    std::printf(">>>> %sFAIL  %s:%d%s\n", ansi_red, result.getSourceInfo().file, result.getSourceInfo().line, ansi_rst);
+                    std::printf("%s%sFAIL  %s:%d%s\n", indent.c_str(), ansi_red, result.getSourceInfo().file, result.getSourceInfo().line, ansi_rst);
                     if (result.hasExpression()) {
-                        std::printf(">>>> %s      %s%s\n", ansi_yel, result.getExpressionInMacro().data(), ansi_rst);
+                        std::printf("%s%s      %s%s\n", indent.c_str(), ansi_yel, result.getExpressionInMacro().data(), ansi_rst);
                     }
                     if (result.hasMessage()) {
-                        std::printf(">>>> %s      %s%s\n", ansi_yel, result.getMessage().data(), ansi_rst);
+                        std::printf("%s%s      %s%s\n", indent.c_str(), ansi_yel, result.getMessage().data(), ansi_rst);
                     }
                     break;
             }

69f6ee2338551349a8c9f525a1aacf93b3d86d30
Pietro Saccardi <lizardm4@gmail.com>
2023-11-05 12:59:10 +0100

    Enable Kconfig for all PN532 ports and channels

--- tests/ut/facility.cpp
+++ tests/ut/facility.cpp
@@ -18,6 +18,189 @@
 
 #define TAG "UT"
 
+// Make sure macros are all defined:
+static_assert(GPIO_NUM_NC == -1, "Macro fallback code relies on GPIO_NUM_NC == -1");
+
+#ifndef CONFIG_PN532_HSU_TX_I2C_SDA
+#define CONFIG_PN532_HSU_TX_I2C_SDA -1
+#else
+static_assert(CONFIG_PN532_HSU_TX_I2C_SDA >= GPIO_NUM_NC and CONFIG_PN532_HSU_TX_I2C_SDA <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_PN532_HSU_RX_I2C_SCL
+#define CONFIG_PN532_HSU_RX_I2C_SCL -1
+#else
+static_assert(CONFIG_PN532_HSU_RX_I2C_SCL >= GPIO_NUM_NC and CONFIG_PN532_HSU_RX_I2C_SCL <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_PN532_SPI_MISO
+#define CONFIG_PN532_SPI_MISO -1
+#else
+static_assert(CONFIG_PN532_SPI_MISO >= GPIO_NUM_NC and CONFIG_PN532_SPI_MISO <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_PN532_SPI_MOSI
+#define CONFIG_PN532_SPI_MOSI -1
+#else
+static_assert(CONFIG_PN532_SPI_MOSI >= GPIO_NUM_NC and CONFIG_PN532_SPI_MOSI <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_PN532_SPI_SCK
+#define CONFIG_PN532_SPI_SCK -1
+#else
+static_assert(CONFIG_PN532_SPI_SCK >= GPIO_NUM_NC and CONFIG_PN532_SPI_SCK <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_PN532_SPI_SS
+#define CONFIG_PN532_SPI_SS -1
+#else
+static_assert(CONFIG_PN532_SPI_SS >= GPIO_NUM_NC and CONFIG_PN532_SPI_SS <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_PN532_I0
+#define CONFIG_PN532_I0 -1
+#else
+static_assert(CONFIG_PN532_I0 >= GPIO_NUM_NC and CONFIG_PN532_I0 <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_PN532_I1
+#define CONFIG_PN532_I1 -1
+#else
+static_assert(CONFIG_PN532_I1 >= GPIO_NUM_NC and CONFIG_PN532_I1 <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_PN532_RSTN
+#define CONFIG_PN532_RSTN -1
+#else
+static_assert(CONFIG_PN532_RSTN >= GPIO_NUM_NC and CONFIG_PN532_RSTN <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_PN532_IRQ
+#define CONFIG_PN532_IRQ -1
+#else
+static_assert(CONFIG_PN532_IRQ >= GPIO_NUM_NC and CONFIG_PN532_IRQ <= GPIO_NUM_MAX);
+#endif
+
+#ifndef CONFIG_SPOOKY_SINGLE_CHANNEL_HSU
+#define CONFIG_SPOOKY_SINGLE_CHANNEL_HSU 0
+#endif
+
+#ifndef CONFIG_SPOOKY_SINGLE_CHANNEL_HSU_IRQ
+#define CONFIG_SPOOKY_SINGLE_CHANNEL_HSU_IRQ 0
+#endif
+
+#ifndef CONFIG_SPOOKY_SINGLE_CHANNEL_I2C
+#define CONFIG_SPOOKY_SINGLE_CHANNEL_I2C 0
+#endif
+
+#ifndef CONFIG_SPOOKY_SINGLE_CHANNEL_I2C_IRQ
+#define CONFIG_SPOOKY_SINGLE_CHANNEL_I2C_IRQ 0
+#endif
+
+#ifndef CONFIG_SPOOKY_SINGLE_CHANNEL_SPI
+#define CONFIG_SPOOKY_SINGLE_CHANNEL_SPI 0
+#endif
+
+#ifndef CONFIG_SPOOKY_SINGLE_CHANNEL_SPI_IRQ
+#define CONFIG_SPOOKY_SINGLE_CHANNEL_SPI_IRQ 0
+#endif
+
+#ifndef CONFIG_SPOOKY_MULTI_CHANNEL
+#define CONFIG_SPOOKY_MULTI_CHANNEL 0
+#endif
+
+#ifndef CONFIG_SPOOKY_MULTI_CHANNEL_HSU
+#define CONFIG_SPOOKY_MULTI_CHANNEL_HSU 0
+#endif
+
+#ifndef CONFIG_SPOOKY_MULTI_CHANNEL_HSU_IRQ
+#define CONFIG_SPOOKY_MULTI_CHANNEL_HSU_IRQ 0
+#endif
+
+#ifndef CONFIG_SPOOKY_MULTI_CHANNEL_I2C
+#define CONFIG_SPOOKY_MULTI_CHANNEL_I2C 0
+#endif
+
+#ifndef CONFIG_SPOOKY_MULTI_CHANNEL_I2C_IRQ
+#define CONFIG_SPOOKY_MULTI_CHANNEL_I2C_IRQ 0
+#endif
+
+#ifndef CONFIG_SPOOKY_MULTI_CHANNEL_SPI
+#define CONFIG_SPOOKY_MULTI_CHANNEL_SPI 0
+#endif
+
+#ifndef CONFIG_SPOOKY_MULTI_CHANNEL_SPI_IRQ
+#define CONFIG_SPOOKY_MULTI_CHANNEL_SPI_IRQ 0
+#endif
+
+#define SPOOKY_ANY_MULTI_CHANNEL (CONFIG_SPOOKY_MULTI_CHANNEL_HSU ||     \
+                                  CONFIG_SPOOKY_MULTI_CHANNEL_HSU_IRQ || \
+                                  CONFIG_SPOOKY_MULTI_CHANNEL_I2C ||     \
+                                  CONFIG_SPOOKY_MULTI_CHANNEL_I2C_IRQ || \
+                                  CONFIG_SPOOKY_MULTI_CHANNEL_SPI ||     \
+                                  CONFIG_SPOOKY_MULTI_CHANNEL_SPI_IRQ)
+
+#define SPOOKY_ANY_SINGLE_CHANNEL (CONFIG_SPOOKY_SINGLE_CHANNEL_HSU ||     \
+                                   CONFIG_SPOOKY_SINGLE_CHANNEL_HSU_IRQ || \
+                                   CONFIG_SPOOKY_SINGLE_CHANNEL_I2C ||     \
+                                   CONFIG_SPOOKY_SINGLE_CHANNEL_I2C_IRQ || \
+                                   CONFIG_SPOOKY_SINGLE_CHANNEL_SPI ||     \
+                                   CONFIG_SPOOKY_SINGLE_CHANNEL_SPI_IRQ)
+
+#define SPOOKY_SINGLE_CHANNEL_COUNT (CONFIG_SPOOKY_SINGLE_CHANNEL_HSU +     \
+                                     CONFIG_SPOOKY_SINGLE_CHANNEL_HSU_IRQ + \
+                                     CONFIG_SPOOKY_SINGLE_CHANNEL_I2C +     \
+                                     CONFIG_SPOOKY_SINGLE_CHANNEL_I2C_IRQ + \
+                                     CONFIG_SPOOKY_SINGLE_CHANNEL_SPI +     \
+                                     CONFIG_SPOOKY_SINGLE_CHANNEL_SPI_IRQ)
+
+#if (CONFIG_SPOOKY_MULTI_CHANNEL && !SPOOKY_ANY_MULTI_CHANNEL) || (!CONFIG_SPOOKY_MULTI_CHANNEL && !SPOOKY_ANY_SINGLE_CHANNEL)
+#error "You must define at least one communication channel."!
+#endif
+
+#if CONFIG_SPOOKY_MULTI_CHANNEL != (SPOOKY_ANY_MULTI_CHANNEL && !SPOOKY_ANY_SINGLE_CHANNEL)
+#error "You must either select some multi channel options, or one single channel option."
+#endif
+
+#if SPOOKY_SINGLE_CHANNEL_COUNT > 1
+#error "You can define at most one single-channel option"
+#endif
+
+#undef SPOOKY_ANY_MULTI_CHANNEL
+#undef SPOOKY_ANY_SINGLE_CHANNEL
+#undef SPOOKY_SINGLE_CHANNEL_COUNT
+
+#if CONFIG_SPOOKY_MULTI_CHANNEL_HSU || CONFIG_SPOOKY_SINGLE_CHANNEL_HSU ||             \
+        CONFIG_SPOOKY_MULTI_CHANNEL_HSU_IRQ || CONFIG_SPOOKY_SINGLE_CHANNEL_HSU_IRQ || \
+        CONFIG_SPOOKY_MULTI_CHANNEL_I2C || CONFIG_SPOOKY_SINGLE_CHANNEL_I2C ||         \
+        CONFIG_SPOOKY_MULTI_CHANNEL_I2C_IRQ || CONFIG_SPOOKY_SINGLE_CHANNEL_I2C_IRQ
+#if CONFIG_PN532_HSU_RX_I2C_SCL <= -1 || CONFIG_PN532_HSU_TX_I2C_SDA <= -1
+#error "You must specify valid HSU TX, RX/I2C SDA, SCL pins to use HSU/I2C, respectively."
+#endif
+#endif
+
+#if CONFIG_SPOOKY_MULTI_CHANNEL_SPI || CONFIG_SPOOKY_SINGLE_CHANNEL_SPI || \
+        CONFIG_SPOOKY_MULTI_CHANNEL_SPI_IRQ || CONFIG_SPOOKY_SINGLE_CHANNEL_SPI_IRQ
+#if CONFIG_PN532_SPI_MISO <= -1 || CONFIG_PN532_SPI_MOSI <= -1 || \
+        CONFIG_PN532_SPI_SCK <= -1 || CONFIG_PN532_SPI_SS <= -1
+#error "You must specify valid SPI MOSI, MISO, SCK and SS pins to use SPI."
+#endif
+#endif
+
+#if CONFIG_SPOOKY_MULTI_CHANNEL_HSU_IRQ || CONFIG_SPOOKY_SINGLE_CHANNEL_HSU_IRQ ||     \
+        CONFIG_SPOOKY_MULTI_CHANNEL_I2C_IRQ || CONFIG_SPOOKY_SINGLE_CHANNEL_I2C_IRQ || \
+        CONFIG_SPOOKY_MULTI_CHANNEL_SPI_IRQ || CONFIG_SPOOKY_SINGLE_CHANNEL_SPI_IRQ
+#if CONFIG_PN532_IRQ <= -1
+#error "You must specify valid IRQ line to use HSU, I2C or SPI with IRQ."
+#endif
+#endif
+
+#if CONFIG_SPOOKY_MULTI_CHANNEL
+#if CONFIG_PN532_I0 <= -1 || CONFIG_PN532_I1 <= -1 || CONFIG_PN532_RSTN <= -1
+#error "You must control the I0, I1 and RSTN lines of the PN532 to enable switching between channels."
+#endif
+#endif
+
 namespace ut {
 
     using pn532::wakeup_source;
@@ -26,140 +209,25 @@ namespace ut {
     namespace {
 
         namespace pinout {
-
-#ifndef PN532_SERIAL_RX
-            constexpr gpio_num_t pn532_hsu_rx = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_hsu_rx = static_cast<gpio_num_t>(PN532_SERIAL_RX);
-#endif
-
-#ifndef PN532_SERIAL_TX
-            constexpr gpio_num_t pn532_hsu_tx = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_hsu_tx = static_cast<gpio_num_t>(PN532_SERIAL_TX);
-#endif
-
-#ifndef PN532_I2C_SCL
-            constexpr gpio_num_t pn532_i2c_scl = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_i2c_scl = static_cast<gpio_num_t>(PN532_I2C_SCL);
-#endif
-
-#ifndef PN532_I2C_SDA
-            constexpr gpio_num_t pn532_i2c_sda = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_i2c_sda = static_cast<gpio_num_t>(PN532_I2C_SDA);
-#endif
-
-#ifndef PN532_SPI_MISO
-            constexpr gpio_num_t pn532_spi_miso = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_spi_miso = static_cast<gpio_num_t>(PN532_SPI_MISO);
-#endif
-
-#ifndef PN532_SPI_MOSI
-            constexpr gpio_num_t pn532_spi_mosi = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_spi_mosi = static_cast<gpio_num_t>(PN532_SPI_MOSI);
-#endif
-
-#ifndef PN532_SPI_SCK
-            constexpr gpio_num_t pn532_spi_sck = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_spi_sck = static_cast<gpio_num_t>(PN532_SPI_SCK);
-#endif
-
-#ifndef PN532_SPI_SS
-            constexpr gpio_num_t pn532_spi_ss = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_spi_ss = static_cast<gpio_num_t>(PN532_SPI_SS);
-#endif
-
-#ifndef PN532_I0
-            constexpr gpio_num_t pn532_cicd_i0 = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_cicd_i0 = static_cast<gpio_num_t>(PN532_I0);
-#endif
-
-#ifndef PN532_I1
-            constexpr gpio_num_t pn532_cicd_i1 = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_cicd_i1 = static_cast<gpio_num_t>(PN532_I1);
-#endif
-
-#ifndef PN532_RSTN
-            constexpr gpio_num_t pn532_cicd_rstn = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_cicd_rstn = static_cast<gpio_num_t>(PN532_RSTN);
-#endif
-
-#ifndef PN532_IRQ
-            constexpr gpio_num_t pn532_irq = GPIO_NUM_NC;
-#else
-            constexpr gpio_num_t pn532_irq = static_cast<gpio_num_t>(PN532_IRQ);
-#endif
+            constexpr gpio_num_t pn532_hsu_rx_i2c_scl = static_cast<gpio_num_t>(CONFIG_PN532_HSU_RX_I2C_SCL);
+            constexpr gpio_num_t pn532_hsu_tx_i2c_sda = static_cast<gpio_num_t>(CONFIG_PN532_HSU_TX_I2C_SDA);
+            constexpr gpio_num_t pn532_spi_miso = static_cast<gpio_num_t>(CONFIG_PN532_SPI_MISO);
+            constexpr gpio_num_t pn532_spi_mosi = static_cast<gpio_num_t>(CONFIG_PN532_SPI_MOSI);
+            constexpr gpio_num_t pn532_spi_sck = static_cast<gpio_num_t>(CONFIG_PN532_SPI_SCK);
+            constexpr gpio_num_t pn532_spi_ss = static_cast<gpio_num_t>(CONFIG_PN532_SPI_SS);
+            constexpr gpio_num_t pn532_cicd_i0 = static_cast<gpio_num_t>(CONFIG_PN532_I0);
+            constexpr gpio_num_t pn532_cicd_i1 = static_cast<gpio_num_t>(CONFIG_PN532_I1);
+            constexpr gpio_num_t pn532_cicd_rstn = static_cast<gpio_num_t>(CONFIG_PN532_RSTN);
+            constexpr gpio_num_t pn532_irq = static_cast<gpio_num_t>(CONFIG_PN532_IRQ);
         }// namespace pinout
 
-#ifdef SPOOKY_I2C
-        constexpr bool supports_i2c = true;
-#else
-        constexpr bool supports_i2c = false;
-#endif
-#ifdef SPOOKY_I2C_IRQ
-        constexpr bool supports_i2c_irq = true;
-#else
-        constexpr bool supports_i2c_irq = false;
-#endif
+        constexpr bool supports_hsu = static_cast<bool>(CONFIG_SPOOKY_MULTI_CHANNEL_HSU || CONFIG_SPOOKY_SINGLE_CHANNEL_HSU);
+        constexpr bool supports_hsu_irq = static_cast<bool>(CONFIG_SPOOKY_MULTI_CHANNEL_HSU_IRQ || CONFIG_SPOOKY_SINGLE_CHANNEL_HSU_IRQ);
+        constexpr bool supports_i2c = static_cast<bool>(CONFIG_SPOOKY_MULTI_CHANNEL_I2C || CONFIG_SPOOKY_SINGLE_CHANNEL_I2C);
+        constexpr bool supports_i2c_irq = static_cast<bool>(CONFIG_SPOOKY_MULTI_CHANNEL_I2C_IRQ || CONFIG_SPOOKY_SINGLE_CHANNEL_I2C_IRQ);
+        constexpr bool supports_spi = static_cast<bool>(CONFIG_SPOOKY_MULTI_CHANNEL_SPI || CONFIG_SPOOKY_SINGLE_CHANNEL_SPI);
+        constexpr bool supports_spi_irq = static_cast<bool>(CONFIG_SPOOKY_MULTI_CHANNEL_SPI_IRQ || CONFIG_SPOOKY_SINGLE_CHANNEL_SPI_IRQ);
 
-#ifdef SPOOKY_SPI
-        constexpr bool supports_spi = true;
-#else
-        constexpr bool supports_spi = false;
-#endif
-#ifdef SPOOKY_SPI_IRQ
-        constexpr bool supports_spi_irq = true;
-#else
-        constexpr bool supports_spi_irq = false;
-#endif
-
-
-#ifdef SPOOKY_HSU
-        constexpr bool supports_hsu = true;
-#else
-        constexpr bool supports_hsu = false;
-#endif
-
-#define PN532_ASSERT_DEFINED_PIN(PIN_MACRO) \
-    static_assert(PIN_MACRO > GPIO_NUM_NC and PIN_MACRO < GPIO_NUM_MAX, "You did not define macro " #PIN_MACRO " (must be a valid GPIO pin).")
-
-#if defined(SPOOKY_I2C) || defined(SPOOKY_I2C_IRQ)
-        PN532_ASSERT_DEFINED_PIN(PN532_I2C_SCL);
-        PN532_ASSERT_DEFINED_PIN(PN532_I2C_SDA);
-#endif
-
-#if defined(SPOOKY_SPI) || defined(SPOOKY_SPI_IRQ)
-        PN532_ASSERT_DEFINED_PIN(PN532_SPI_MISO);
-        PN532_ASSERT_DEFINED_PIN(PN532_SPI_MOSI);
-        PN532_ASSERT_DEFINED_PIN(PN532_SPI_SCK);
-        PN532_ASSERT_DEFINED_PIN(PN532_SPI_SS);
-#endif
-
-#if defined(SPOOKY_SPI_IRQ) || defined(SPOOKY_I2C_IRQ)
-        PN532_ASSERT_DEFINED_PIN(PN532_IRQ);
-#endif
-
-#if defined(SPOOKY_HSU)
-        PN532_ASSERT_DEFINED_PIN(PN532_SERIAL_TX);
-        PN532_ASSERT_DEFINED_PIN(PN532_SERIAL_RX);
-#endif
-
-#if defined(SPOOKY_CI_CD_MACHINE)
-        PN532_ASSERT_DEFINED_PIN(PN532_I0);
-        PN532_ASSERT_DEFINED_PIN(PN532_I1);
-        PN532_ASSERT_DEFINED_PIN(PN532_RSTN);
-#endif
-
-#undef PN532_ASSERT_DEFINED_PIN
         constexpr uart_config_t uart_config = {
                 .baud_rate = 115200,
                 .data_bits = UART_DATA_8_BITS,
@@ -171,8 +239,8 @@ namespace ut {
 
         constexpr i2c_config_t i2c_config = {
                 .mode = I2C_MODE_MASTER,
-                .sda_io_num = pinout::pn532_i2c_sda,
-                .scl_io_num = pinout::pn532_i2c_scl,
+                .sda_io_num = pinout::pn532_hsu_tx_i2c_sda,
+                .scl_io_num = pinout::pn532_hsu_rx_i2c_scl,
                 .sda_pullup_en = GPIO_PULLUP_ENABLE,
                 .scl_pullup_en = GPIO_PULLUP_ENABLE,
                 .master = {.clk_speed = 400000},
@@ -217,7 +285,7 @@ namespace ut {
         : _channel{nullptr},
           _controller{nullptr},
           _active_channel{channel_type::none} {
-#ifdef SPOOKY_CI_CD_MACHINE
+#if CONFIG_SPOOKY_MULTI_CHANNEL
         gpio_set_direction(pinout::pn532_cicd_rstn, GPIO_MODE_OUTPUT);
         gpio_set_direction(pinout::pn532_cicd_i0, GPIO_MODE_OUTPUT);
         gpio_set_direction(pinout::pn532_cicd_i1, GPIO_MODE_OUTPUT);
@@ -234,14 +302,14 @@ namespace ut {
             desfire::esp32::suppress_log suppress{PN532_TAG};
             _controller->power_down({wakeup_source::i2c, wakeup_source::hsu, wakeup_source::spi});
         }
-#ifdef SPOOKY_CI_CD_MACHINE
+#if CONFIG_SPOOKY_MULTI_CHANNEL
         gpio_set_level(pinout::pn532_cicd_rstn, 0);
 #endif
         std::this_thread::sleep_for(200ms);
     }
 
     void facility::power_up() {
-#ifdef SPOOKY_CI_CD_MACHINE
+#if CONFIG_SPOOKY_MULTI_CHANNEL
         gpio_set_level(pinout::pn532_cicd_rstn, 1);
 #endif
         std::this_thread::sleep_for(200ms);
@@ -281,7 +349,7 @@ namespace ut {
     }
 
     bool facility::activate_internal(ut::channel_type ct) {
-#ifdef SPOOKY_CI_CD_MACHINE
+#if CONFIG_SPOOKY_MULTI_CHANNEL
         // Configure I0/I1 for the selected mode
         switch (ct) {
             case channel_type::hsu:
@@ -308,7 +376,7 @@ namespace ut {
 
         switch (ct) {
             case channel_type::hsu:
-                _channel = std::make_unique<pn532::esp32::hsu_channel>(UART_NUM_1, uart_config, pinout::pn532_hsu_tx, pinout::pn532_hsu_rx);
+                _channel = std::make_unique<pn532::esp32::hsu_channel>(UART_NUM_1, uart_config, pinout::pn532_hsu_tx_i2c_sda, pinout::pn532_hsu_rx_i2c_scl);
                 break;
             case channel_type::i2c:
                 _channel = std::make_unique<pn532::esp32::i2c_channel>(I2C_NUM_0, i2c_config);
